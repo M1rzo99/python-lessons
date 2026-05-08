c52a5854a067986e89e811dc56f777997c061928
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-05-08 17:35:19 +0900

    python-darslar
    python o'tganishni boshladim, boshlang'ich darslar

---
+++ hello-world.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  8 14:10:15 2026
+
+@author: mirzo
+"""
+#  dars vaziafalri
+
+print("Hello world!")
+print(1+3/2)
+print("Hayirli tong")
+print(2-4*2)
+print(91/5)
+print(2**4)
+print(2++2)
+print(2//2)
+
+# Print funksiyasi:
+    
+print("Print \"do\" Function") # agarda biz printni ichidagi biror gapni qo'shtirnoq ichiga olmoqchi bo'lsak
+
+
+print("""agar matnni ikki 
+va undan ko'p  qatorga ajratmoqchi bo'lsak
+uchta ochilib,yopiladigan qo'shtirnoq ishlatamiz.""")
+
+print("Agarda biz matnni  \nikki va undan ortiq qatorga joylashtirmoqchi bo'lsak (back slash(\) + n) ishlatamiz")
+
+print('bitta tirnoq ishlatsak va o\'qi,o\'g\'il dagi tirnoqlar oldidan back slash(ishlatamiz)')
