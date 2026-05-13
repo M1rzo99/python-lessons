d7c0337e73a544efc70d6a2450dfba5370dcab1f
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-05-13 23:28:19 +0900

    LIST

---
+++ LIST.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 23:03:57 2026
+
+# List
+
+@author: mirzo
+
+"""
+
+mevalar = ["o'rik","olma","behi","uzum"]
+sonlar = [12000,34000,45000,67000]
+son_harf = [21,34,"Mirzo","uzum",-45]
+bosh = [ ]
