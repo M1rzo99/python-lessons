c5e4899e70cd255f8d11f0edda9487289b259923
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-05-15 20:09:00 +0900

    for

---
+++ for.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 19:22:54 2026
+
+@author: mirzo
+"""
+
+# for tsiki pastdagui ko'rinishda bo'ladi. 
+#print esa obzasdan yoziladi.
+mehmonlar = ["Ali","Vali", "BEkzod","Jumali"]
+for mehmon in mehmonlar: 
+    print("Salom",mehmon)
+    print("Hayr",mehmon)
+    
+
