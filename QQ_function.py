33810fa5e1ab5c786b097cef0e87888e0a2222f9
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-06-15 22:53:27 +0900

    #amaliyot2

--- QQ_function.py
+++ QQ_function.py
@@ -8,87 +8,86 @@
 
 # Qiymat qaytaruvchi funksiya
 
-def toliq_ism_yasa(ism,familya):
-    """ Toliq  ism qaytaruvchi funksiya"""
-    toliq_ism = f"{ism} {familya}"
-    return toliq_ism
-user1 = toliq_ism_yasa("Mirzo", "Shomuratov")
-user2 = toliq_ism_yasa("Asliddin", "Uzoqov")
-print(user2,user1)
+# def toliq_ism_yasa(ism,familya):
+#     """ Toliq  ism qaytaruvchi funksiya"""
+#     toliq_ism = f"{ism} {familya}"
+#     return toliq_ism
+# user1 = toliq_ism_yasa("Mirzo", "Shomuratov")
+# user2 = toliq_ism_yasa("Asliddin", "Uzoqov")
+# print(user2,user1)
 
-# Return  - o'zgaruvchining qiymatini qaytaradi.
+# # Return  - o'zgaruvchining qiymatini qaytaradi.
 
-# Ixtiyoriy argumentlar - parametrga kiritgan malumotni yoki argumentini beramiz, yokida bermaymiz
+# # Ixtiyoriy argumentlar - parametrga kiritgan malumotni yoki argumentini beramiz, yokida bermaymiz
 
-def oila_ism_yasa(ism,familya,oilada_kim=""):
-    """ Oila azosi nomi va oilada kimligi ko'rsatuvchi function"""
-    if oilada_kim:#oiladagi rolini tekshiramiz
-        oilada_azo = f"{ism} {familya} {oilada_kim}"
-    else:
-        oilada_azo = f"{ism} {familya}"
-    return oilada_azo.title()
-shaxs1 = oila_ism_yasa("Aziza", "SHomuratova")
-print(shaxs1)
+# def oila_ism_yasa(ism,familya,oilada_kim=""):
+#     """ Oila azosi nomi va oilada kimligi ko'rsatuvchi function"""
+#     if oilada_kim:#oiladagi rolini tekshiramiz
+#         oilada_azo = f"{ism} {familya} {oilada_kim}"
+#     else:
+#         oilada_azo = f"{ism} {familya}"
+#     return oilada_azo.title()
+# shaxs1 = oila_ism_yasa("Aziza", "SHomuratova")
+# print(shaxs1)
 
-# Funksiyadan Lug'at qaytaramiz
+# # Funksiyadan Lug'at qaytaramiz
 
-def avto_info(komponiya,model,rangi,karobka,yili,narx=None):
-    avto ={
-        'komponiya':komponiya,
-        'model':model,
-        'rangi':rangi,
-        'karobka':karobka,
-        'yili':yili,
-        'narx':narx
-        }
-    return avto
-avto1 = avto_info("KIA", "K9", "Qora", "Avtomat", 2025,220000)
-avtolar = [avto1]
-print("Onlayn  bozordagi mavjud mashinalar:")
-for avto in avtolar:
-    if avto["narx"]:
-        narx = avto["narx"]
-    else:
-        narx = "Nomalum"
-    print(f"{avto['model']}, {avto['komponiya']}. Narxi:{narx}")
+# def avto_info(komponiya,model,rangi,karobka,yili,narx=None):
+#     avto ={
+#         'komponiya':komponiya,
+#         'model':model,
+#         'rangi':rangi,
+#         'karobka':karobka,
+#         'yili':yili,
+#         'narx':narx
+#         }
+#     return avto
+# avto1 = avto_info("KIA", "K9", "Qora", "Avtomat", 2025,220000)
+# avtolar = [avto1]
+# print("Onlayn  bozordagi mavjud mashinalar:")
+# for avto in avtolar:
+#     if avto["narx"]:
+#         narx = avto["narx"]
+#     else:
+#         narx = "Nomalum"
+#     print(f"{avto['model']}, {avto['komponiya']}. Narxi:{narx}")
 
-# Funksiyadadn ro'yhat qaytaramiz
-# oraliq nomli function yasaymiz va functionimiz 2 son oralig'idagi sonlarni ro'yhat ko'rinishda qaytaradi.
-def oraliq(min,max,qadam):
-    sonlar=[]
-    while min<max:
-        sonlar.append(min)
-        min += qadam
-    return sonlar
-print(oraliq(0,11,2))
+# # Funksiyadadn ro'yhat qaytaramiz
+# # oraliq nomli function yasaymiz va functionimiz 2 son oralig'idagi sonlarni ro'yhat ko'rinishda qaytaradi.
+# def oraliq(min,max,qadam):
+#     sonlar=[]
+#     while min<max:
+#         sonlar.append(min)
+#         min += qadam
+#     return sonlar
+# print(oraliq(0,11,2))
 
-# FUNKSIYALARNI TSIKLDA ISHLATISH
-print("Saytimizdagi avtolar ro'yxatini shakllantiramiz.")
-avtolar=[] # salondagi avtolar uchun bo'sh ro'yxat
-while True:
-    print("\nQuyidagi ma'lumotlarni kiriting",end='')
-    kompaniya=input("Ishlab chiqaruvchi: ")
-    model=input("Modeli: ")
-    rangi=input("Rangi: ")
-    korobka=input("Korobka: ")
-    yili=input("Ishlab chiqarilgan yili: ")
-    narhi=input("Narhi: ")
+# # FUNKSIYALARNI TSIKLDA ISHLATISH
+# print("Saytimizdagi avtolar ro'yxatini shakllantiramiz.")
+# avtolar=[] # salondagi avtolar uchun bo'sh ro'yxat
+# while True:
+#     print("\nQuyidagi ma'lumotlarni kiriting",end='')
+#     kompaniya=input("Ishlab chiqaruvchi: ")
+#     model=input("Modeli: ")
+#     rangi=input("Rangi: ")
+#     korobka=input("Korobka: ")
+#     yili=input("Ishlab chiqarilgan yili: ")
+#     narhi=input("Narhi: ")
     
-    #Foydalanuvchi kiritdan ma'lumotlardan avto_info yordamida 
-    #lug'at shakllantirib, har bir lug'atni ro'yxatga qo'shamiz:
-    avtolar.append(avto_info(kompaniya, model, rangi, korobka, yili, narhi))
+#     #Foydalanuvchi kiritdan ma'lumotlardan avto_info yordamida 
+#     #lug'at shakllantirib, har bir lug'atni ro'yxatga qo'shamiz:
+#     avtolar.append(avto_info(kompaniya, model, rangi, korobka, yili, narhi))
     
-    # Yana avto qo'shish-qo'shmaslikni so'raymiz
-    javob = input("Yana avto qo'shasizmi? (yes/no): ")
-    if javob=='no':
-        break
-print(avtolar)
+#     # Yana avto qo'shish-qo'shmaslikni so'raymiz
+#     javob = input("Yana avto qo'shasizmi? (yes/no): ")
+#     if javob=='no':
+#         break
+# print(avtolar)
 
 
 # Amaliyot
 
 #1. Foydanaluvchidan ismi, familiyasi, tug'ilgan yili, tug'ilgan joyi, email manzili va telefon raqamini qabul qilib, lug'at ko'rinishida qaytaruvchi funksiya yozing. Lug'atda foydalanuvchu yoshi ham bo'lsin. Ba'zi argumentlarni kiritishni ixtiyoriy qiling (masalan, tel.raqam, el.manzil
-
 
 def mijoz_info(ism, familiya, tyil, tjoy, email="", tel=None):
     """Mijoz haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
@@ -103,9 +102,31 @@ def mijoz_info(ism, familiya, tyil, tjoy, email="", tel=None):
     }
     return mijoz
 
+print("Mijoz haqida ma'lumotlarni kiriting.")
+mijozlar =[]
+while True:
+    ism = input("Ismi: ")
+    familiya = input("Familiyasi: ")
+    telefon = input("Telefon raqami: ")
+    mijozlar.append(mijoz_info(ism, familiya, tyil, tjoy, email, telefon))
+    javob = input("Davom etasizmi? (ha/yo'q)")
+    if javob!='ha':
+        break
 
-
-
+print("Mijozlar:")
+for mijoz in mijozlar:
+    print(f"{mijoz['ism'].title()} {mijoz['familiya'].title()}," 
+          f"{mijoz['yoshi']} yoshda, telefoni: {mijoz['telefon']}")
+   
+#2.Yuqoridagi funksiyani while yordamida bir necha bor chaqiring, va mijozlar degan ro'yxatni shakllantiring. Ro'yxatdagi mijozlar haqidagi ma'lumotni konsolga chiqaring.
+def kattasi(x,y,z):
+    max = x
+    if y>=max:
+        max = y
+    if z>=max:
+        max = z
+    return max
+   
 
 
 
