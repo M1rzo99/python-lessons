0e50b4c3916e4b04945883b08596ef1094d8703e
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-06-18 06:39:13 +0900

    #6.Amaliyot

--- QQ_function.py
+++ QQ_function.py
@@ -170,6 +170,18 @@ def tub_sonlar_top(min, max):
 
     return tub_sonlar
 
+#6.Amaliyot 6
 
+def fibonacci(n):
+    sonlar = []
+    for x in range(n):
+        if x == 0 or x == 1:
+            sonlar.append(1)
+        else:
+            sonlar.append(sonlar[x - 1] + sonlar[x - 2])
+    return sonlar
+
+
+print(fibonacci(10))
 
 
