86647fded7e8b30fd114817969b67d5336926ca2
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-06-17 23:34:32 +0900

    Amaliyot 4

--- QQ_function.py
+++ QQ_function.py
@@ -139,6 +139,18 @@ print(ismlar)
 
 print(yangi_ismlar)
 
+#4. Amaliyot 4
+
+
+def aylana_info(radius, pi=3.14159):
+    aylana = {
+        "radius": radius,
+        "diametr": 2 * radius,
+        "perimetr": 2 * radius * pi,
+        "yuza": pi * radius ** 2,
+    }
+    return aylana
+
 
 
 
