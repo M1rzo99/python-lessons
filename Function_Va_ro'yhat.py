b2d621cfe29ead57aef4e4769ee8834d851e3a73
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-06-19 06:42:45 +0900

    #2.Amaliyot

--- Function_Va_ro'yhat.py
+++ Function_Va_ro'yhat.py
@@ -50,7 +50,18 @@ ismlar = ["ali", "vali", "hasan", "husan"]
 katta_harf(ismlar)
 print(ismlar)
 
+#2.Amaliyot
+def katta_harf(matnlar):
+    matnlar = matnlar[:]
+    for i in range(len(matnlar)):
+        matnlar[i] = matnlar[i].title()
+    return matnlar
 
+
+ismlar = ["ali", "vali", "hasan", "husan"]
+yangi_ismlar = katta_harf(ismlar)
+print(ismlar)
+print(yangi_ismlar)
 
 
 
