b7a9c2ccea9808ba3e3bf005fb7ec0f517c11487
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-06-18 22:45:01 +0900

    # ASL RO'YXATGA O'ZGARTIRISH KIRITISHNING OLDINI OLISH:

--- Function_Va_ro'yhat.py
+++ Function_Va_ro'yhat.py
@@ -25,3 +25,15 @@ print(baholar)
 #Yuqoridagi funksiya unga uzatilgan ro'yxat ichidagi talabalarning ismini 
 #.pop() yordamida sug'urib olgani uchun bizning asl ro'yxatimiz ham bo'shab qoldi. 
 # E'tibor bering, funksiya tashqarisidagi va ichidagi ro'yxatlar ikki hil nomlangan bo'lsada (talabalar va ismlar), ikkalasi ham xotiradagi bitta ro'yxatga bog'langani sabab ulardan biriga o'zgartirish kiritilishi bilan, ikkinchisi ham o'zgaradi. 
+
+
+# ASL RO'YXATGA O'ZGARTIRISH KIRITISHNING OLDINI OLISH:
+# Ro'yhatdan nusxa olish un:  [:] dan foydalanamiz
+#Agar funksiya asl ro'yxatga o'zgartirish kiritishini istamasangiz, funksiyaga ro'yxatning o'zini emas, uning nusxasini uzatish mumkin.
+# Buning uchun funksiya parametrini royxat_nomi[:] ko'rinishida yozish kifoya. 
+#Bunda [:] operatori ro'yxatdan nusxa olishni bildiradi:
+print("Ro'yxatdan nusxa olishni bildiradi:")
+talabalar = ['ali', 'vali', 'hasan', 'husan']
+baholar = bahola(talabalar[:])
+print(talabalar)
+    
