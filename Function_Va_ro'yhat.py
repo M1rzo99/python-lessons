f0ee8f013f30322dd274d62dd8eca2921f61e6c3
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-06-18 22:36:37 +0900

    # Funksiyaga ro'yhat uzatish

--- Function_Va_ro'yhat.py
+++ Function_Va_ro'yhat.py
@@ -6,61 +6,18 @@
 # @author: mirzo
 # """
 
+# Funksiyaga ro'yhat uzatish
 
-# FUNKSIYAGA RO'YXAT UZATISH
-# Biz avvalgi darslarimizda funksiyaga parametr sifatida yagona qiymat berayotgan edik. Aslida, bu bilan cheklanmasdan, funksiyaga ro'yxat (list) ham berishimiz mumkin. Bunda, funksiya ro'yxat qiymatlariga to'g'ridan-to'g'ri murojat qila oladi.
-# Keling talabalarni baholaydigan funksiya yozamiz. Funksiyamiz talabalar ro'yxatini qabul qilib oladi, ro'yxatdan har bir talabani sug'urib olib (.pop()), bahosini kiritishni so'raydi. Talaba ismi va bahosini lug'atga joylab, yakuniy lug'atni foydalanuvchiga qaytaradi.
+def bahola(ismlar):
+    baholar = {}
+    while ismlar:
+        ism = ismlar.pop()
+        baho = input(f"Talaba {ism.title()}ning bahosini kiriting: ")
+        baholar[ism]=baho
+    return baholar
 
-# Copy
-# def bahola(ismlar):
-#     baholar = {}
-#     while ismlar:
-#         ism = ismlar.pop()
-#         baho = input(f"Talaba {ism.title()}ning bahosini kiriting: ")
-#         baholar[ism]=baho
-#     return baholar
+talabalar = ['ali','vali','hasan','husan']
+baholar = bahola(talabalar)
+print(baholar)
 
-# talabalar = ['ali', 'vali', 'hasan', 'husan']
-# baholar = bahola(talabalar)
-# print(baholar)
- 
-
-# Natija
-# RO'YXATGA O'ZGARTIRISH KIRITISH
-# Funksiyaga ro'yxat uzatganimizda, funksiya ro'yxat elementlariga to'g'ridan-to'g'ri murojat qila oladi. Ro'yxatga funksiya ichida kiritilgan o'zgartirishlar asl ro'yxatga ham ta'sir qiladi. Avvalgi misolimizga qaytaylik:
-
-# Copy
-# talabalar = ['ali', 'vali', 'hasan', 'husan']
-# baholar = bahola(talabalar)
-# print(talabalar)
-# Natija: []
-# Yuqoridagi funksiya unga uzatilgan ro'yxat ichidagi talabalarning ismini .pop() yordamida sug'urib olgani uchun bizning asl ro'yxatimiz ham bo'shab qoldi. E'tibor bering, funksiya tashqarisidagi va ichidagi ro'yxatlar ikki hil nomlangan bo'lsada (talabalar va ismlar), ikkalasi ham xotiradagi bitta ro'yxatga bog'langani sabab ulardan biriga o'zgartirish kiritilishi bilan, ikkinchisi ham o'zgaradi. 
-
-# Ikki o'zgaruvchi ham bitta ro'yxatga bog'langan
-# ASL RO'YXATGA O'ZGARTIRISH KIRITISHNING OLDINI OLISH 
-# Agar funksiya asl ro'yxatga o'zgartirish kiritishini istamasangiz, funksiyaga ro'yxatning o'zini emas, uning nusxasini uzatish mumkin. Buning uchun funksiya parametrini royxat_nomi[:] ko'rinishida yozish kifoya. Bunda [:] operatori ro'yxatdan nusxa olishni bildiradi:
-
-# Copy
-# talabalar = ['ali', 'vali', 'hasan', 'husan']
-# baholar = bahola(talabalar[:])
-# print(talabalar)
-# Natija: ['ali', 'vali', 'hasan', 'husan']
-# AMALIYOT
-# Matnlardan iborat ro'yxat qabul qilib, ro'yxatdagi har bir matnning birinchi harfini katta harfga o'zgatiruvchi funksiya yozing. 
-
-# Copy
-# ismlar = ['ali', 'vali', 'hasan', 'husan']
-# katta_harf(ismlar)
-# print(ismlar)
-# Kutilgan natija: ['Ali', 'Vali', 'Hasan', 'Husan']
-# Yuoqirdagi funksiyani asl ro'yxatni o'zgartirmaydigan va yangi ro'yxat qaytaradigan qilib o'zgartiring
-
-# Copy
-# ismlar = ['ali', 'vali', 'hasan', 'husan']
-# yangi_ismlar = katta_harf(ismlar)
-# print(ismlar)
-# print(yangi_ismlar)
-# Kutilgan natija: 
-# ['ali', 'vali', 'hasan', 'husan'] 
-# ['Ali', 'Vali', 'Hasan', 'Husan']
-# Darsimiz davomida yozgan bahola funksiyasini .pop() metodidan foydalanmasdan va asl ro'yxatga o'zgartirish kiritmasdan faqat lug'at qaytaradigan qilib yozing.
+#
