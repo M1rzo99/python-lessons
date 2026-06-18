72186db3424f479fdeaa22a518277a39d1f982d0
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-06-18 22:40:19 +0900

    # Ro'yhatga o'zgartirish kiritish

--- Function_Va_ro'yhat.py
+++ Function_Va_ro'yhat.py
@@ -20,4 +20,8 @@ talabalar = ['ali','vali','hasan','husan']
 baholar = bahola(talabalar)
 print(baholar)
 
-#
+# Ro'yhatga o'zgartirish kiritish
+# pop() - yordamida ro'yhatdan har bir elementni sug'urib ola bilamiz.
+#Yuqoridagi funksiya unga uzatilgan ro'yxat ichidagi talabalarning ismini 
+#.pop() yordamida sug'urib olgani uchun bizning asl ro'yxatimiz ham bo'shab qoldi. 
+# E'tibor bering, funksiya tashqarisidagi va ichidagi ro'yxatlar ikki hil nomlangan bo'lsada (talabalar va ismlar), ikkalasi ham xotiradagi bitta ro'yxatga bog'langani sabab ulardan biriga o'zgartirish kiritilishi bilan, ikkinchisi ham o'zgaradi. 
