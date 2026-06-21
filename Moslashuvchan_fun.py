df12a63d74d4c02ac5d36753c623d1ba7704cad3
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-06-21 21:39:45 +0900

    # oraliq degan function. range functionni qo'lda yasaymiz

--- Moslashuvchan_fun.py
+++ Moslashuvchan_fun.py
@@ -18,4 +18,14 @@ def summa(*sonlar):
     for son in sonlar:
         yigindi +=son
     return yigindi
-print(summa(10,12))
+print(summa(10,12)) 
+
+# oraliq degan function. range functionni qo'lda yasaymiz
+def oraliq (min,max):
+    sonlar=[]
+    while min<max: # qachonki mindagi son max dagi sondan kichik bo'lsa, pastdagi jarayon ishlasin
+        sonlar.append(min) # sonlar nomli listga qo'shadi,qachonki mindagi son maaxdagi songa teng bo'lmaguncha.
+        min +=1 # while tsikli har safar aylanganda unga +1 qo'shilsin
+    return sonlar # va sonlar nomli listni qaytarsin(boshqa joylarda ham ishlatsa bo'ladigan qb)
+print(oraliq(0,11))
+
