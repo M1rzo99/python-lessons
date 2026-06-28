8caf8e7ff9016216ebe36db9f3aec9559a9cbacc
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-06-28 21:24:04 +0900

    #filter() FUNKSIYASI

--- Funksiyalar_songgi_soz.py
+++ Funksiyalar_songgi_soz.py
@@ -95,3 +95,59 @@ Copy
 ismlar = ['hasan','husan','olim','umid']
 print(list(map(lambda matn:matn.upper(),ismlar)))
 Natija: ['HASAN', 'HUSAN', 'OLIM', 'UMID']
+
+
+
+
+#filter() FUNKSIYASI
+Bu funksiya ham argument sifatida ro'yxat va boshqa funskiyani qabul qilib oladi va berilgan ro'yxat elementlarini berilgan funksiya yordamida saralaydi. Bunda argument sifatida uzatilgan funksiya mantiqiy qiymat qaytarishi kerak (True yoki False).
+Keling bunga ham bir misol ko'ramiz: tasodifiy sonlar ro'yxatidan juft sonalrni ajratib oluvchi dastur yozamiz. Dasturimiz 3 qismdan iborat:
+Avvalo, random modulidagi sample() funksiyasi yordamida 0-99 oralig'idagi 10 ta tasodifiy sonlar ro'yxatini tuzib oldik
+Berilgan son juft (True) yoki juft emas (False) ekanligini qaytaruvchi funksiya yozdik
+filter() fuksiyasiga yangi yaratgan juftmi funksiyasi va tasodifiy sonlar ro'yxatini uzatib, yangi juft_sonlar ro'yxatini shakllantridik
+
+Copy
+import random as r
+
+sonlar = r.sample(range(100),10) # 0-99 oralig'ida 10 ta tasodifiy sonlar
+
+def juftmi(x):
+    """x juft bo'lsa True, aks holda False qaytaruvchu funksiya"""
+    return x%2==0
+
+juft_sonlar = list(filter(juftmi,sonlar))
+print(sonlar)
+print(juft_sonlar)
+
+Natija
+Keling endi shu dasturni lambda yordamida yozamiz:
+
+Copy
+import random as r
+
+sonlar = r.sample(range(100),10) # 0-99 oralig'ida 10 ta tasodifiy sonlar
+juft_sonlar = list(filter(lambda son: son%2==0,sonlar))
+
+print(sonlar)
+print(juft_sonlar)
+
+Natija
+Kurib turganingizdek, lambda funksiya yordamida dastur bir muncha qisqaroq chiqadi. Agar juftmi funksiyasi kelajakda shart bo'lmasa, alohida funksiya yaratib o'tirmasdan, bir marttalik lambda funksiyasidan foydalangan afzal.
+Keling endi filter() funksiyasi yordamida matnlarni saralashga ham misollar ko'raylik.
+Quyidagi dastur mevalar ro'yxatidan b harfiga boshlanuvchi mevalarni ajratib oladi. Bu yerda biz matnlarga tegishli bo'lgan .startswith() metodidan foydalandik. Bu metod, berilgan matn shu harfdan boshlanadimi yoki yo'q tekshiradi va True yoki False qiymat qaytaradi.
+
+Copy
+mevalar = ['olma','anor','anjir','shaftoli',"o'rik","tarvuz","qovun","banan"]
+
+mevalar_b = list(filter(lambda meva:meva.startswith('b'),mevalar))
+print(mevalar_b)
+Quyidagi dastur esa mevalar ro'yxatidan nomi 5 yoki undan kam harfdan iborat mevalarni saralab oladi.
+
+Copy
+mevalar2 = list(filter(lambda meva:len(meva)<=5, mevalar))
+print(mevalar2)
+Natija: ['olma', 'anor', 'anjir', "o'rik", 'qovun', 'banan']
+Topingchi, quyidagi kod qanday vazifani bajaradi?
+
+Copy
+list(filter(lambda meva:(meva.startswith('a') and meva.endswith('r')), mevalar))
