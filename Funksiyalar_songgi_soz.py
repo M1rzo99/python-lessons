5daccbe8967fec5408b274df1ab32f78ff74a81b
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-06-28 21:23:07 +0900

    #lambda YOHUD NOMSIZ FUNKSIYA

---
+++ Funksiyalar_songgi_soz.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 28 21:22:15 2026
+
+@author: mirzo
+"""
+
+#lambda YOHUD NOMSIZ FUNKSIYA
+Pythonning o'ziga xos xususiyatlaridan biri, nomsiz vaqtinchalik funksiyalar yaratish imkoniyati. Bunday funksiyalarni yaratishda def operatori o'rniga lambda operatori ishlatilgani uchun ham lambda funskiyalar deb ataladi. 
+Nomsiz funksiyalar quyidagicha yaratiladi:
+
+Copy
+lambda argument:ifoda
+Lambda funksiyalari istalgan miqdordagi argumentlarga ega bo'lishi mumkin, ammo funksiya badanida faqat bitta ifoda mavjud bo'ladi. Ifoda bajariladi va qaytariladi (return operatori shart emas).
+Nomsiz funksiyalar biror ifodani tezda hisoblab olishda qulay. Misol uchun quyidgai lambda funksiya ikkita argument qabul qiladi ( 
+p
+i
+,
+r
+pi,r ), va aylana uzunligini qaytaradi: 
+
+Copy
+import math
+uzunlik = lambda pi, r : 2*pi*r
+print(uzunlik(math.pi,10))
+Natija: 62.83185307179586
+Kodni tahlil qilamiz, 1-qatorda math modulini chaqirib oldik. 2-qatorda lambda funksiyani yaratdik, funksiyamiz pi va r argumentlarini qabul qilib, 2*pi*r qiymatni qaytaradi. Funksiyaga nom bermadik, lekin unga uzunlik identifikatori orqali murojat qilishimiz mumkin. 3-qatorda funksiyamizga murojat qildik va natijani konsolga chiqardik.
+Yana bir misol, topingchi quyidagi funksiyaning vazfiasi nima?
+
+Copy
+product = lambda x, y : x ** y
+print(product(3, 2))
+ Shu yerda so'rashingiz mumkin, nima uchun lambda nomsiz deb ataladi, ahir unga hozirgina nomi bilan murojat qildikku?
+Gap shundaki, lambda finksiyalarning asl mohiyati boshqa funskiyalar bilan birga ishlaganda ko'rinadi. Keling, tushunarli bo'lishi uchun oddiyroq misol ko'ramiz.
+Quyidagi dasturda biz avval daraja degan funksiya yasadik, bu funskiyamiz n degan o'zgaruvchi qabul qilib oladi va funksiya ichidagi noma'lum x ning n-darajasini qaytaradi. Aslida daraja bu funksiya yasaydigan funksya bo'ldi. Xo'sh, undan qanday foydalanamiz? 4-5-qatorlarda esa daraja funksiyasidan yana 2 ta funksiya yasadik: kvadrat - kiritilgan sonning kvadratini hisoblaydi, kub - kiritilgan sonning kubini hisoblaydi.
+
+Copy
+def daraja(n):
+    return lambda x : x**n
+
+kvadrat = daraja(2)
+kub = daraja(3)
+print(f"3-ning kvadrati {kvadrat(3)} ga, kubi {kub(3)} ga teng")
+Natija: 3-ning kvadrati 9 ga, kubi 27 ga teng
+Lambda funksiyalaridan argument sifatida boshqa funksyani qabul qiluvchi funksiyalar bilan ishlashda ham keng foydalaniladi. Misol uchun map() va filter() funksiyalari.
