1e72fe8635b0cdabf0a1713ab257751f63686f08
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-06-30 23:00:50 +0900

    # OOP KIRISH

---
+++ OOP.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 30 23:00:25 2026
+
+@author: mirzo
+"""
+
+# OOP KIRISH
+Dasturlashni o’rganar ekanmiz, albatta object oriented programming (OOP) ya’ni obyektga yo’naltiriglan dasturlash tushunchasiga kelamiz. Ko’pchilik uchun bu bosqich biroz tushunarsiz va murakkabdek tuyuladi. Aslida unday emas. Keling bugun object oriented programming va unga tegishli tamoyillar haqida gaplashamiz.
+KLASSIK YOHUD CHIZIQLI DASTURLASH
+OOPni tushunish uchun avval klassik dasturlashni ko’raylik. Gap shundaki ilk kompyuterlar va dasturlar matematik muammolarni hal qilishga qaratilgan. Bunday dasturlar foydalanuvchidan biror ma’lumotlarni qabul qilib olgan, va qati’iy ketma-ketlik ya’ni tartibga amal qilgan holda turli arifmetik amallarni bajarib, dastur so’ngida foydalanuvchi kutgan natijani qaytargan. Shuning uchun ham bunday dasturlar chiziqli yoki tartibli dasturlar deb ataladi.
+
+Chiziqli dasturlash
+Siz ham dasturlashga ilk qadam qo’yganingizda mana shunday chiziqli dasturlarni yozishni o’rganishdan boshlaysiz.
+ Sizning dasturingiz bir nechta o’zgaruvchilar va funksiyalardan iborat bo’ladi.
+ Bu o’zgaruvchilar va funksiyalar ma’lum ketma-ketlikda bir biri bilan munosabatga kiradi va dastur yakunida siz kutgan natijani beradi.
+
+Chiziqli dastur tarkibi va algoritmi
+Dastur kattalashgani sari o’zgaruvchilar va funksiyalar soni ortib boradi. Ular o’rtasidagi munosabatlar ham chigallashib, kodingiz murakkab va tushunishga qiyin bo’lib ketadi. 
+Dasturlash jarayonida bitta funksiyaga o’zgartirish kiritishingiz esa, unga bo’gliq boshqa funksiylaraning ishdan chiqishiga va dasturingiz xato natija berishiga olib kelishi ham mumkin. 
+
+Chiziqli dastur murakkablashib ketishi mumkin
+Chiziqli dasturlarning afzalliklari
+:
+Dasturlashni o’rganish uchun qulay
+Sodda va tushunarli kod
+Dastur algoritmini kuzatish oson
+Dastur xotirada kamroq joy egallaydi
+
+Chiziqli dasturlarning kamchiliklari
+:
+Murakkab dasturlarni chziqili usulda yozish qiyin (ilojsiz)
+Bir dastur uchun yozilgan koddan boshqa dasturda qayta foydalanib bo’lmaydi
+Dastur ichidagi ma’lumotlar (o’zgaruvchilar) barcha funksiyalar uchun ochiq
+ZAMONAVIY DASTURLAR CHIZIQLI EMAS
+Vaqt o’tib dasturlarga qo’yilgan talablar murakkablashib borgani sababli, chiziqli dasturlash tamoyili zamon talabiga javob bermay qo’ydi va 1970 yillarda object oriented programming tamoyili olg’a surila boshlandi.
