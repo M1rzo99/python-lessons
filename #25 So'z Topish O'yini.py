50db8648748338b0e91f86ace9ffba827baa73be
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-07-03 16:06:41 +0900

    Tasodifiy_sonlar_o'yini(sontop_function)

---
+++ #25 So'z Topish O'yini.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 15:11:44 2026
+Son topish o'yini
+
+@author: mirzo
+"""
+
+import random # tasodifiy sonlarni generate qilishda yordam beradi
+
+def sontop(x=10):# qiymat qabul qiladi va komputer o'ylaydigan sonlarni yuqori chegarasidir(x)
+    tasodifiy_son = random.randint(1, x) # berilgan sonlar orasidagi butun sonni qaytaradi
+    print(f"Men 1 dan {x} gacha son o'yladim.Topa olasizmi?")
+    taxminlar = 0
+    while True:
+       taxminlar +=1
+       taxmin = int(input(">>>"))
+       if taxmin<tasodifiy_son:
+           print("Xato.Men o'ylagan son bundan kattaroq.Yana harakat qilib ko'ring:")
+       elif taxmin>tasodifiy_son:
+           print("Xato.Men o'ylagan son bundan kichikroq.Yana harakat qilib ko'ring:")
+       else:
+           break
+    print(f"Tabriklayman, {taxminlar} ta taxmin bilan to'g'ri topdingiz.")
+    return taxminlar
+
+
+ 
