a74fe7ef231063b212de34ef5b446f7dbf357b4c
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-07-09 13:12:04 +0900

    #27.OOP_Object: Proporties and Methods

---
+++ #27.OOP.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 30 23:00:25 2026
+
+@author: mirzo
+"""
+
+# OOP - oBjectga yo'naltirilgan dasturlash tushunchasi.
+
+# Klassik(chiziqli) dasturlash.
+
+# Ilk komputerlar va dasturlar,matematik muammolarni yechishga qaratilgan.
+#Bunday dasturlar foydalanuvchcidan biror malumiotni qabul qilib oladi va qatiy ketma-ketlikda yani tartibga amal qilgan holda, turli arifmetik amallar bajarib,dastur so'ngida foydalaniuvchi kutgan natijasni qaytaradi.
+# shuning un ham bunday dastur,chiziqli yoki tartibli dastur ddi.
+
+# Chiziqli dasturlarning afzalliklari:
+    
+# Dasturlashni o’rganish uchun qulay
+# Sodda va tushunarli kod
+# Dastur algoritmini kuzatish oson
+# Dastur xotirada kamroq joy egallaydi
+
+
+# Chiziqli dasturlarning kamchiliklari:
+    
+# Murakkab dasturlarni chziqili usulda yozish qiyin (ilojsiz)
+# Bir dastur uchun yozilgan koddan boshqa dasturda qayta foydalanib bo’lmaydi
+# Dastur ichidagi ma’lumotlar (o’zgaruvchilar) barcha funksiyalar uchun ochiq
+# ZAMONAVIY DASTURLAR CHIZIQLI EMAS.
+
+
+# 1970 - yillarda Object Oriented Programming tamoyili olg'a surila boshladi.
+
+# Object - Dasturlashda o'zaro bog'liq bo'lgan o'zgaruvchilar va functionlar bitta konteynerga jamlanadi va bunday konteynerlar OBJECT ddi.
+#Proportiers - Bir obyektga tegishli o'zgaruvchilarnig xususiyatlari,unga tegishli funksiyalar esa methodlar ddi.
+
+#Agar real dasturdan misol keltiradigan bo’lsak, istalgan dastur ichidagi tugma bu obyekt. 
+#Uning shakli, rangi va matni esa xususiyatlari bo’ladi. Tugma ustida bajariladigan amallar tugmaning metodlari deyiladi.
+#Misol uchun tugmani bosish, uzoq bosish, ustiga sichqonchani olib borish va hokazo.
+
+
+# Object Orinted Dasturlar o'nlab balkim yuzlab objectlardan iborat bo'ladi. Va bunday dastular uchun dastur boshi yoki oxiri degn tushuncha nisbiy bo'ladi.
+# Object Oriented dastrular bajarilishida qatiy ketam-ketlik amal qilmaydi.Foydalanuvchi istalgan objectga murojat qilishi,uning ustida amallar bajarishi mn.
+# Bitta Objectga murojat ortidan,boshqa object ham faollashishi mn.
+
+
+#Misol uchun, mobil ilovalarda obyektlar bu dastur ichidagi tugmalar, matnlar, rasmlar va boshqa elementlardir. Foydalanuvchi istalgan tugmani bosishi, istalgan matnni ajratib olishi va boshqa amallarni istalgan tartibda bajarishi mumkin. Bunda bitta tugma (ya’ni obyektni) bosish bulan boshqa obyekt (masalan rasm) o’zgarishi mumkin.
+#Zamonaviy kompyuter o’yinlari ham minglab obyektlardan iborat. Foydalanuvchi esa virtual o’yin olamida erkin harakat qilishi, istlagan tarafga yurishi, istalgan vaqtda turli obyektlar bilan turli amallar bajarishi mumkin. 
+
+
+
+
+
+
+
+
