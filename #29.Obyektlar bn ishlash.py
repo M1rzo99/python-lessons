5cf78f367d4de93557739973ad5334ee10b3ec33
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-07-14 10:45:40 +0900

    # Yana boshqa usuli,obyekt xususiyatlarini yangilovchi method yaratish:

--- #29.Obyektlar bn ishlash.py
+++ #29.Obyektlar bn ishlash.py
@@ -30,7 +30,57 @@ class Sabzavotlar:
         self.turi = turi
         self.kilo = kilo
         self.narx = 2000
+        self.yet_joy = "Katta dalada yetishtilgan"
     def get_inf(self):
-        return f"{self.nomi} {self.turi}, {self.kilo}. Uning narxi {self.narx} won."
+        return f"{self.nomi} {self.turi}, {self.kilo}.{self.yet_joy}, Uning narxi {self.narx} won."
 maxsulot1 = Sabzavotlar("Piyoz", "Qizil", "5kg")
 print(maxsulot1.get_inf())
+
+
+# Standart qiymatni o'zgartirish:
+# Obyektnnig standart qiymatioga ham boshqa xususiyatlar kabi nuqta orqali murojat qilishimiz va uning qiymatini almashtirishmiz mumkin:
+talaba1.bosqich = 2
+talaba1.ism = "Mirzo"
+print(talaba1.get_info())
+
+# Yana boshqa usuli,obyekt xususiyatlarini yangilovchi method yaratish:
+
+
+class Talaba:
+    """ Talaba nomli class Yaratayapmiz"""
+    def __init__(self,ism,familya,Tyil):
+        """Talabaning xususiyatlari"""
+        self.ism = ism
+        self.familya = familya
+        self.Tyil = Tyil
+        self.bosqich = 1 # Xususiyatga standart qiymat berish shudir
+    def get_info(self):
+        return f"{self.ism} {self.familya} {self.Tyil}-yil, {self.bosqich}-bosqich talabasi"
+    
+    def set_bosqich(self,bosqich):
+        self.bosqich = bosqich
+        
+        
+talaba1 = Talaba("Jumanazar", "Shokirov", "2002")
+talaba1.set_bosqich(10)
+print(talaba1.get_info())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
