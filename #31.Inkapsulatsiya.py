c22c4ec15c9cd1a5f05b51c4fac16dcf9c8d1069
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-07-15 22:41:17 +0900

    # KLASSNING XUSUSIYATLARI VA METODLARI

--- #31.Inkapsulatsiya.py
+++ #31.Inkapsulatsiya.py
@@ -1,22 +1,166 @@
-# #!/usr/bin/env python3
-# # -*- coding: utf-8 -*-
-# """
-# Created on Wed Jul 15 22:39:23 2026
+# # #!/usr/bin/env python3
+# # # -*- coding: utf-8 -*-
+# # """
+# # Created on Wed Jul 15 22:39:23 2026
 
-# @author: mirzo
-# """
+# # @author: mirzo
+# # """
 
-# INKAPSULYATSIYA
-# Obyektga Yo'naltirilgan Dasturlashning tamoyillaridan biri bu inkapsulyatsiya, ya'ni obyektning xususiyatlarga to'g'ridan-to'g'ri (nuqta orqali) murojat qilishni va ularning qiymatini o'zgartirishni taqiqlab qo'yish. Pythonda bunday yopiq xususiyatlarning nomi ikki pastki chiziq bilan boshlanadi:
+# # INKAPSULYATSIYA
+# # Obyektga Yo'naltirilgan Dasturlashning tamoyillaridan biri bu inkapsulyatsiya, ya'ni obyektning xususiyatlarga to'g'ridan-to'g'ri (nuqta orqali) murojat qilishni va ularning qiymatini o'zgartirishni taqiqlab qo'yish. Pythonda bunday yopiq xususiyatlarning nomi ikki pastki chiziq bilan boshlanadi:
+
+# # Copy
+# # from uuid import uuINKAPSULYATSIYA
+# # Obyektga Yo'naltirilgan Dasturlashning tamoyillaridan biri bu inkapsulyatsiya, ya'ni obyektning xususiyatlarga to'g'ridan-to'g'ri (nuqta orqali) murojat qilishni va ularning qiymatini o'zgartirishni taqiqlab qo'yish. Pythonda bunday yopiq xususiyatlarning nomi ikki pastki chiziq bilan boshlanadi:
+
+# # Copy
+# # from uuid import uuid4
+# # class Avto:
+# #     """Avtomobil klassi"""
+# #     def __init__(self,make,model,rang,yil,narh,km=0):
+# #         """Avtomobilning xususiyatlari"""
+# #         self.make = make
+# #         self.model = model
+# #         self.rang = rang
+# #         self.yil = yil
+# #         self.narh = narh
+# #         self.__km = km
+# #         self.__id = uuid4()
+    
+# #     def get_km(self):
+# #         return self.__km
+    
+# #     def get_id(self):
+# #         return self.__id
+# # Yuqoridagi kodimizning 11-qatorida __km xususiyati avtomobilning necha km yurgani haqida ma'lumot saqlaydi va bu ma'lumotni tashqaridan o'zgartirib bo'lmaydi. Kodimizning 12-qatorida esa har bir yangi yaratilgan avtomobilga yangi, noyob va takrorlanmas ID generasiya qilish uchun uuid4() funksiyasidan foydalanayapmiz. Deylik biz mashinalar sotish uchun onlayn bozor yaratsak, bozorimizga qo'shilgan har bir moshina endi o'zining ID raqamiga ega bo'ladi va bu ID raqamni to'g'ridan-to'g'ri (nuqta orqali) ko'rib bo'lmaydi.
+
+# # Copy
+# # avto1 = Avto("GM","Malibu","Qora",2020,40000,100000)
+# # avto1.__km
+# #  Natija: AttributeError: 'Avto' object has no attribute '__km'
+# # Yopiq xususiyatlarni ko'rish uchun esa alohida metodlar yozish maqsadga muvofiq bo'ladi (get_km() va get_id()):
+
+# # Copy
+# # print(f"ID: {avto1.get_id()}")
+# # Natija: ID: 1d4f39a4-3222-4682-9231-6275ca5e1bff
+# # Bunday yopiq xususiyatlarni o'zgartirish ham metodlar orqali amalga oshirilishi kerak. Misol uchun mashinaning necha km yurganini o'zgartirish uchun klassimizga quyidagi metodni qo'shamiz:
+
+# # Copy
+# #     def add_km(self,km):
+# #         """Mashinaning km ga yana km qo'shish"""
+# #         if km>=0:
+# #             self.__km += km
+# #         else:
+# #             print("Mashina km kamaytirib bo'lmaydi")
+
+# # Copy
+# # avto1.add_km(1500)
+# # print(avto1.get_km())
+# # Natija: 101500
+# # Inkapsulyatsiyaning maqsadi obyektning ma'lum xususiyatlarini tashqi ta'sirdan himoya qilish. Misol uchun yuqoridagi misolimizda mashinaning qancha yurganini faqat musbat tarafga o'zgartirish mumkin, noyob ID raqamini esa umuman o'zgartirib bo'lmaydi.id4
+# # class Avto:
+# #     """Avtomobil klassi"""
+# #     def __init__(self,make,model,rang,yil,narh,km=0):
+# #         """Avtomobilning xususiyatlari"""
+# #         self.make = make
+# #         self.model = model
+# #         self.rang = rang
+# #         self.yil = yil
+# #         self.narh = narh
+# #         self.__km = km
+# #         self.__id = uuid4()
+    
+# #     def get_km(self):
+# #         return self.__km
+    
+# #     def get_id(self):
+# #         return self.__id
+# # Yuqoridagi kodimizning 11-qatorida __km xususiyati avtomobilning necha km yurgani haqida ma'lumot saqlaydi va bu ma'lumotni tashqaridan o'zgartirib bo'lmaydi. Kodimizning 12-qatorida esa har bir yangi yaratilgan avtomobilga yangi, noyob va takrorlanmas ID generasiya qilish uchun uuid4() funksiyasidan foydalanayapmiz. Deylik biz mashinalar sotish uchun onlayn bozor yaratsak, bozorimizga qo'shilgan har bir moshina endi o'zining ID raqamiga ega bo'ladi va bu ID raqamni to'g'ridan-to'g'ri (nuqta orqali) ko'rib bo'lmaydi.
+
+# # Copy
+# # avto1 = Avto("GM","Malibu","Qora",2020,40000,100000)
+# # avto1.__km
+# #  Natija: AttributeError: 'Avto' object has no attribute '__km'
+# # Yopiq xususiyatlarni ko'rish uchun esa alohida metodlar yozish maqsadga muvofiq bo'ladi (get_km() va get_id()):
+
+# # Copy
+# # print(f"ID: {avto1.get_id()}")
+# # Natija: ID: 1d4f39a4-3222-4682-9231-6275ca5e1bff
+# # Bunday yopiq xususiyatlarni o'zgartirish ham metodlar orqali amalga oshirilishi kerak. Misol uchun mashinaning necha km yurganini o'zgartirish uchun klassimizga quyidagi metodni qo'shamiz:
+
+# # Copy
+# #     def add_km(self,km):
+# #         """Mashinaning km ga yana km qo'shish"""
+# #         if km>=0:
+# #             self.__km += km
+# #         else:
+# #             print("Mashina km kamaytirib bo'lmaydi")
+
+# # Copy
+# # avto1.add_km(1500)
+# # print(avto1.get_km())
+# # Natija: 101500
+# # Inkapsulyatsiyaning maqsadi obyektning ma'lum xususiyatlarini tashqi ta'sirdan himoya qilish. Misol uchun yuqoridagi misolimizda mashinaning qancha yurganini faqat musbat tarafga o'zgartirish mumkin, noyob ID raqamini esa umuman o'zgartirib bo'lmaydi.INKAPSULYATSIYA
+# # Obyektga Yo'naltirilgan Dasturlashning tamoyillaridan biri bu inkapsulyatsiya, ya'ni obyektning xususiyatlarga to'g'ridan-to'g'ri (nuqta orqali) murojat qilishni va ularning qiymatini o'zgartirishni taqiqlab qo'yish. Pythonda bunday yopiq xususiyatlarning nomi ikki pastki chiziq bilan boshlanadi:
+
+# # Copy
+# # from uuid import uuid4
+# # class Avto:
+# #     """Avtomobil klassi"""
+# #     def __init__(self,make,model,rang,yil,narh,km=0):
+# #         """Avtomobilning xususiyatlari"""
+# #         self.make = make
+# #         self.model = model
+# #         self.rang = rang
+# #         self.yil = yil
+# #         self.narh = narh
+# #         self.__km = km
+# #         self.__id = uuid4()
+    
+# #     def get_km(self):
+# #         return self.__km
+    
+# #     def get_id(self):
+# #         return self.__id
+# # Yuqoridagi kodimizning 11-qatorida __km xususiyati avtomobilning necha km yurgani haqida ma'lumot saqlaydi va bu ma'lumotni tashqaridan o'zgartirib bo'lmaydi. Kodimizning 12-qatorida esa har bir yangi yaratilgan avtomobilga yangi, noyob va takrorlanmas ID generasiya qilish uchun uuid4() funksiyasidan foydalanayapmiz. Deylik biz mashinalar sotish uchun onlayn bozor yaratsak, bozorimizga qo'shilgan har bir moshina endi o'zining ID raqamiga ega bo'ladi va bu ID raqamni to'g'ridan-to'g'ri (nuqta orqali) ko'rib bo'lmaydi.
+
+# # Copy
+# # avto1 = Avto("GM","Malibu","Qora",2020,40000,100000)
+# # avto1.__km
+# #  Natija: AttributeError: 'Avto' object has no attribute '__km'
+# # Yopiq xususiyatlarni ko'rish uchun esa alohida metodlar yozish maqsadga muvofiq bo'ladi (get_km() va get_id()):
+
+# # Copy
+# # print(f"ID: {avto1.get_id()}")
+# # Natija: ID: 1d4f39a4-3222-4682-9231-6275ca5e1bff
+# # Bunday yopiq xususiyatlarni o'zgartirish ham metodlar orqali amalga oshirilishi kerak. Misol uchun mashinaning necha km yurganini o'zgartirish uchun klassimizga quyidagi metodni qo'shamiz:
+
+# # Copy
+# #     def add_km(self,km):
+# #         """Mashinaning km ga yana km qo'shish"""
+# #         if km>=0:
+# #             self.__km += km
+# #         else:
+# #             print("Mashina km kamaytirib bo'lmaydi")
+
+# # Copy
+# # avto1.add_km(1500)
+# # print(avto1.get_km())
+# # Natija: 101500
+# # Inkapsulyatsiyaning maqsadi obyektning ma'lum xususiyatlarini tashqi ta'sirdan himoya qilish. Misol uchun yuqoridagi misolimizda mashinaning qancha yurganini faqat musbat tarafga o'zgartirish mumkin, noyob ID raqamini esa umuman o'zgartirib bo'lmaydi.
+
+
+
+# KLASSNING XUSUSIYATLARI VA METODLARI
+# Avvalgi darslarimizda biror klassdan yaratilgan har bir obyektning xususiyatlarini klass ichidagidef __init__() medoti yordamida berayotgan edik. Bu metod qanday ishlaydi? Biz har gal klassga murojat qilganimizda klass aynan shu __init__metodini ishga tushiradi va biz bergan xususiyatlar bilan yangi obyekt yaratadi.
+# Pythonda xususiyatlarni nafaqat obyektga balki to'g'ridan-to'g'ri klassga ham yuklash imkoniyati mavjud. Bunda klassning xususiyatiga berilgan qiymat barcha obyektlar uchun umumiy bo'ladi. Bu xususiyatni bir obyekt orqali o'zgartirilganda shu klassga oid barcha obyektlarda ham uning qiymati o'zgaradi.
+# Klassga oid xususiyatlar def __init__() metodidan avval e'lon qilinadi.
+# Keling tushunarli bo'lishi uchun quyidagi misolni ko'ramiz:
 
 # Copy
-# from uuid import uuINKAPSULYATSIYA
-# Obyektga Yo'naltirilgan Dasturlashning tamoyillaridan biri bu inkapsulyatsiya, ya'ni obyektning xususiyatlarga to'g'ridan-to'g'ri (nuqta orqali) murojat qilishni va ularning qiymatini o'zgartirishni taqiqlab qo'yish. Pythonda bunday yopiq xususiyatlarning nomi ikki pastki chiziq bilan boshlanadi:
-
-# Copy
-# from uuid import uuid4
 # class Avto:
 #     """Avtomobil klassi"""
+#     num_avto = 0
 #     def __init__(self,make,model,rang,yil,narh,km=0):
 #         """Avtomobilning xususiyatlari"""
 #         self.make = make
@@ -26,125 +170,20 @@
 #         self.narh = narh
 #         self.__km = km
 #         self.__id = uuid4()
-    
-#     def get_km(self):
-#         return self.__km
-    
-#     def get_id(self):
-#         return self.__id
-# Yuqoridagi kodimizning 11-qatorida __km xususiyati avtomobilning necha km yurgani haqida ma'lumot saqlaydi va bu ma'lumotni tashqaridan o'zgartirib bo'lmaydi. Kodimizning 12-qatorida esa har bir yangi yaratilgan avtomobilga yangi, noyob va takrorlanmas ID generasiya qilish uchun uuid4() funksiyasidan foydalanayapmiz. Deylik biz mashinalar sotish uchun onlayn bozor yaratsak, bozorimizga qo'shilgan har bir moshina endi o'zining ID raqamiga ega bo'ladi va bu ID raqamni to'g'ridan-to'g'ri (nuqta orqali) ko'rib bo'lmaydi.
+#         Avto.num_avto += 1
+#   Kodni tahlil qilamiz:
+# 1-qatroda Avto klassini e'lo qildik
+# 3-qatorda Avto klassiga oid bo'lgan xususiyat num_avto yaratdik va unga 0 qiymatini berdik
+# Keyingi qatorlarda __init__ metodi yordamida klassdan yaratiladigan obyektlarning xususiyatlarini e'lon qildik va har gal bu metdoga murojat qilingandan num_avto ning qiymatini 1 ga oshradigan qilib qo'ydik (13-qator).
+# Yuqoridagi usul bilan endi biz dastur davomida Avto klassidan jami nechta obyektlar yaratilganini kuzatib borishimiz mumkin bo'ladi. Bunda num_avto o'zgaruvchisiga istalgan obyekt orqali yoki klass nomi orqali murojat qilish mumkin:
 
 # Copy
-# avto1 = Avto("GM","Malibu","Qora",2020,40000,100000)
-# avto1.__km
-#  Natija: AttributeError: 'Avto' object has no attribute '__km'
-# Yopiq xususiyatlarni ko'rish uchun esa alohida metodlar yozish maqsadga muvofiq bo'ladi (get_km() va get_id()):
+# avto1 = Avto("GM","Malibu","Qora",2020,40000)
+# avto2 = Avto("GM","Lacetti","Oq",2020,20000)
+# print(avto1.num_avto)
+# Natija: 2
 
 # Copy
-# print(f"ID: {avto1.get_id()}")
-# Natija: ID: 1d4f39a4-3222-4682-9231-6275ca5e1bff
-# Bunday yopiq xususiyatlarni o'zgartirish ham metodlar orqali amalga oshirilishi kerak. Misol uchun mashinaning necha km yurganini o'zgartirish uchun klassimizga quyidagi metodni qo'shamiz:
-
-# Copy
-#     def add_km(self,km):
-#         """Mashinaning km ga yana km qo'shish"""
-#         if km>=0:
-#             self.__km += km
-#         else:
-#             print("Mashina km kamaytirib bo'lmaydi")
-
-# Copy
-# avto1.add_km(1500)
-# print(avto1.get_km())
-# Natija: 101500
-# Inkapsulyatsiyaning maqsadi obyektning ma'lum xususiyatlarini tashqi ta'sirdan himoya qilish. Misol uchun yuqoridagi misolimizda mashinaning qancha yurganini faqat musbat tarafga o'zgartirish mumkin, noyob ID raqamini esa umuman o'zgartirib bo'lmaydi.id4
-# class Avto:
-#     """Avtomobil klassi"""
-#     def __init__(self,make,model,rang,yil,narh,km=0):
-#         """Avtomobilning xususiyatlari"""
-#         self.make = make
-#         self.model = model
-#         self.rang = rang
-#         self.yil = yil
-#         self.narh = narh
-#         self.__km = km
-#         self.__id = uuid4()
-    
-#     def get_km(self):
-#         return self.__km
-    
-#     def get_id(self):
-#         return self.__id
-# Yuqoridagi kodimizning 11-qatorida __km xususiyati avtomobilning necha km yurgani haqida ma'lumot saqlaydi va bu ma'lumotni tashqaridan o'zgartirib bo'lmaydi. Kodimizning 12-qatorida esa har bir yangi yaratilgan avtomobilga yangi, noyob va takrorlanmas ID generasiya qilish uchun uuid4() funksiyasidan foydalanayapmiz. Deylik biz mashinalar sotish uchun onlayn bozor yaratsak, bozorimizga qo'shilgan har bir moshina endi o'zining ID raqamiga ega bo'ladi va bu ID raqamni to'g'ridan-to'g'ri (nuqta orqali) ko'rib bo'lmaydi.
-
-# Copy
-# avto1 = Avto("GM","Malibu","Qora",2020,40000,100000)
-# avto1.__km
-#  Natija: AttributeError: 'Avto' object has no attribute '__km'
-# Yopiq xususiyatlarni ko'rish uchun esa alohida metodlar yozish maqsadga muvofiq bo'ladi (get_km() va get_id()):
-
-# Copy
-# print(f"ID: {avto1.get_id()}")
-# Natija: ID: 1d4f39a4-3222-4682-9231-6275ca5e1bff
-# Bunday yopiq xususiyatlarni o'zgartirish ham metodlar orqali amalga oshirilishi kerak. Misol uchun mashinaning necha km yurganini o'zgartirish uchun klassimizga quyidagi metodni qo'shamiz:
-
-# Copy
-#     def add_km(self,km):
-#         """Mashinaning km ga yana km qo'shish"""
-#         if km>=0:
-#             self.__km += km
-#         else:
-#             print("Mashina km kamaytirib bo'lmaydi")
-
-# Copy
-# avto1.add_km(1500)
-# print(avto1.get_km())
-# Natija: 101500
-# Inkapsulyatsiyaning maqsadi obyektning ma'lum xususiyatlarini tashqi ta'sirdan himoya qilish. Misol uchun yuqoridagi misolimizda mashinaning qancha yurganini faqat musbat tarafga o'zgartirish mumkin, noyob ID raqamini esa umuman o'zgartirib bo'lmaydi.INKAPSULYATSIYA
-# Obyektga Yo'naltirilgan Dasturlashning tamoyillaridan biri bu inkapsulyatsiya, ya'ni obyektning xususiyatlarga to'g'ridan-to'g'ri (nuqta orqali) murojat qilishni va ularning qiymatini o'zgartirishni taqiqlab qo'yish. Pythonda bunday yopiq xususiyatlarning nomi ikki pastki chiziq bilan boshlanadi:
-
-# Copy
-# from uuid import uuid4
-# class Avto:
-#     """Avtomobil klassi"""
-#     def __init__(self,make,model,rang,yil,narh,km=0):
-#         """Avtomobilning xususiyatlari"""
-#         self.make = make
-#         self.model = model
-#         self.rang = rang
-#         self.yil = yil
-#         self.narh = narh
-#         self.__km = km
-#         self.__id = uuid4()
-    
-#     def get_km(self):
-#         return self.__km
-    
-#     def get_id(self):
-#         return self.__id
-# Yuqoridagi kodimizning 11-qatorida __km xususiyati avtomobilning necha km yurgani haqida ma'lumot saqlaydi va bu ma'lumotni tashqaridan o'zgartirib bo'lmaydi. Kodimizning 12-qatorida esa har bir yangi yaratilgan avtomobilga yangi, noyob va takrorlanmas ID generasiya qilish uchun uuid4() funksiyasidan foydalanayapmiz. Deylik biz mashinalar sotish uchun onlayn bozor yaratsak, bozorimizga qo'shilgan har bir moshina endi o'zining ID raqamiga ega bo'ladi va bu ID raqamni to'g'ridan-to'g'ri (nuqta orqali) ko'rib bo'lmaydi.
-
-# Copy
-# avto1 = Avto("GM","Malibu","Qora",2020,40000,100000)
-# avto1.__km
-#  Natija: AttributeError: 'Avto' object has no attribute '__km'
-# Yopiq xususiyatlarni ko'rish uchun esa alohida metodlar yozish maqsadga muvofiq bo'ladi (get_km() va get_id()):
-
-# Copy
-# print(f"ID: {avto1.get_id()}")
-# Natija: ID: 1d4f39a4-3222-4682-9231-6275ca5e1bff
-# Bunday yopiq xususiyatlarni o'zgartirish ham metodlar orqali amalga oshirilishi kerak. Misol uchun mashinaning necha km yurganini o'zgartirish uchun klassimizga quyidagi metodni qo'shamiz:
-
-# Copy
-#     def add_km(self,km):
-#         """Mashinaning km ga yana km qo'shish"""
-#         if km>=0:
-#             self.__km += km
-#         else:
-#             print("Mashina km kamaytirib bo'lmaydi")
-
-# Copy
-# avto1.add_km(1500)
-# print(avto1.get_km())
-# Natija: 101500
-# Inkapsulyatsiyaning maqsadi obyektning ma'lum xususiyatlarini tashqi ta'sirdan himoya qilish. Misol uchun yuqoridagi misolimizda mashinaning qancha yurganini faqat musbat tarafga o'zgartirish mumkin, noyob ID raqamini esa umuman o'zgartirib bo'lmaydi.
+# avto3 = Avto("Toyota",'Carolla',"Silver",2018, 45000)
+# print(Avto.num_avto)
+#   Natija: 3
