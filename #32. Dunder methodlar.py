5d73fa37e8d1fab4b06b60801506416ce948f902
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-07-20 23:30:04 +0900

    #OBYEKT HAQIDA MA'LUMOT

--- #32. Dunder methodlar.py
+++ #32. Dunder methodlar.py
@@ -43,4 +43,57 @@ Created on Mon Jul 20 23:28:37 2026
  'narh',
  'rang',
  'yil']
-Dunder metodlardan biz __init__ metodi bilan tanishdik. Bu metod klassdan obyekt yaratishda chaqiriladi va obyektning xususiyatlarini belgilaydi. Ushbu darsimizda esa maxsus metodlarning ba'zilari bilan tanishamiz.
+#Dunder metodlardan biz __init__ metodi bilan tanishdik. Bu metod klassdan obyekt yaratishda chaqiriladi va obyektning xususiyatlarini belgilaydi. Ushbu darsimizda esa maxsus metodlarning ba'zilari bilan tanishamiz.
+
+
+
+
+#OBYEKT HAQIDA MA'LUMOT
+#Obyektga print() yoki str() orqali murojat qilinganda obyekt haqida tushunarli ma'lumot qaytarish uchun __repr__va __str__ metodlaridan foydalanamiz. Tushunarli bo'lishi uchun avvalgi darsimizdagi Avto klassiga qaytamiz:
+
+
+class Avto:
+    __num_avto = 0
+    """Avtomobil klassi"""
+    def __init__(self,make,model,rang,yil,narh):
+        """Avtomobilning xususiyatlari"""
+        self.make = make
+        self.model = model
+        self.rang = rang
+        self.yil = yil
+        self.narh = narh
+        Avto.__num_avto += 1
+Yuqoridagi klassdan yangi obyekt yaratamiz va obyekt haqida ma'lumot olish uchun print() funksiyasini chaqiramiz:
+
+Copy
+avto1 = Avto("GM","Malibu","Qora",2020,40000)
+print(avto1) # obyekt haqida ma'lumot
+Natija: <__main__.Avto object at 0x00000238A6DAE0C8> 
+Qandaydur tushunarsiz ma'lumot. Ekrandagi natijadan biz faqat avto1 obyektimiz Avto klassiga tegishli ekanini ko'ramiz. Qanday qilib shuning o'rniga obyekt haqida tushunarliroq ma'lumot olishimiz mumkin?
+Gap shundaki biz har gal obyketga print() (yoki str() yoki repr()) orqali murojat qilganimizda, Python obyket ichida __str__ yoki __repr__ metodlariga murojat qiladi. Agar biz bu metodlarni yozmagan bo'lsak, yuqoridagi kabi umumiy ma'lumot qayataradi. 
+Biz ushbu metodlarni yangidan yozib, biz istagan ma'lumotni qayataradian qilishimiz mumkin. Odatda, yuqoridagi ikki metoddan birini yozish kifoya. Odatda, __repr__ umumiyorq, __str__ esa batafsilroq ma'lumot olish uchun ishlatiladi. 
+Ikkalasidan birini tanlaganda, __repr__metodiga yon bosiladi, sababi bu metod print(), str() va repr() funksiyalarining hammasi bilan ishlaydi. Keling biz ham yuoqirdagi klassimizga__repr__metodini qo'shamiz:
+
+Copy
+class Avto:
+    __num_avto = 0
+    """Avtomobil klassi"""
+    def __init__(self,make,model,rang,yil,narh):
+        """Avtomobilning xususiyatlari"""
+        self.make = make
+        self.model = model
+        self.rang = rang
+        self.yil = yil
+        self.narh = narh
+        Avto.__num_avto += 1
+    
+    def __repr__(self):
+        """Obyekt haqida ma'lumot"""
+        return f"Avto: {self.rang} {self.make} {self.model}"
+Qaytadan print() funksiyasini chaqiramiz:
+
+Copy
+avto1 = Avto("GM","Malibu","Qora",2020,40000)
+print(avto1)
+Natija: Avto: Qora GM Malibu
+Mana endi natijamiz ancha tushunarli ko'rinishda chiqdi.
