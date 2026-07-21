6bc13ba3b842c14e0235b5ea5cdae27422f47ade
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-07-21 12:38:51 +0900

    # Klassing xususiyatlari va Methodlari:

--- #31.Inkapsulatsiya.py
+++ #31.Inkapsulatsiya.py
@@ -41,14 +41,49 @@ avto1.add_km(-1000)
 print(avto1.get_km())
 
 
+# Inkapsulyatsiyaning maqasdadi obyektni malum bir xususiyatalrnini tashqi ta'sirdanhmoya qilish.
+# Misol un yuqoridagi misolda mashinaning qancha km yurganini faqat musbat tarafga o'zgartirish mumkin.Noyob ID ni esa umuman o'zgartirib bo'lmaydi.
 
 
+# Klassing xususiyatlari va Methodlari:
 
+    # avvalgi darslarda biz biror klassdan yaratilgan har bir obyektning xususiyatlarini klass ichidagi def__init__() methodi yordamida berayotgan edik.
+    # Bunda method qanday ishlaydi: Biz har gal classga murojat qilayotganimizda aynan shu __init__ methodini ishga tushiradi va biz bergan xususiyatlar bn yangi obyekt yaratilinadi.
+    
+# Pythonda xususiyatlarni nafaqat obyektga balikm to'g'ridan-to'g'ri classga ham yuklash imkoniyati bor.Bunda klassning xususiyatlariga berilgan qiymat barcha obyektlar uchun umumiy bo'ladi.
+# Bu xususiyatni bur obyekt orqali o'zgartirganda shu klassga oid barcha obyektlarda ham uning qiyamti o'zgaradi.
 
+# Classga oid xususiyatlar dev__init__ methodidan oldin e'lon qilinadi
+class Avto:
+    """Avtomobil klassi"""
+    num_avto = 0 #Classga xususiyat berdik
+    
+    def __init__(self,make,model,rang,yil,narh,km=0):
+        """Avtomobilning xususiyatlari"""
+        self.make = make
+        self.model = model
+        self.rang = rang
+        self.yil = yil
+        self.narh = narh
+        self.__km = km
+        self.__id = uuid4()
+        Avto.num_avto += 1
 
+# Kod tajlil qilamiz:
+    # 1- qatorda Avto klassini yasadik
+    # 3- qatorda Avto klassiga oid bo'lgan xususiyat num_avto yaratdik va unga 0 qiymatini berdik
+    # Keyingi qatorlarda __init__ methodi yordamida klassdan yaratilgan obyektlarning xususiyatlarini e'lon qildik,
+    # va har gal bu methodga murojat qilinganda, num_avto ning qiymatini 1 ga oshiradigan qilib qo'ydik
 
+# Yuqoridagi usul bn biz endi dastur davomida Avto klasidan jami nechta obyektlar yaratilganini kuzatob borishimiz mn.
+# Bunda num_avto o'zgaruvchisiga istalgan obyekt nomi yoki klass nomi orqali murojat qilish mn:
+    
+avto11 = Avto("Hyundai", "Grande", "Blue", 2021, 30000)
+avto22 = Avto("Hyundai", "Grande", "Blue", 2021, 30000)
+avto33 = Avto("Hyundai", "Grande", "Blue", 2021, 30000)
+avto44 = Avto("Hyundai", "Grande", "Blue", 2021, 30000)
 
-
+print(avto11.num_avto)#  Natija 4, chunki 4 ta obyekt yaratdik va 4 marta chaqirdik 
 
 
 
