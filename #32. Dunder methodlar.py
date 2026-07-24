ca7bc1ad64c89f44b3ca39dfafb0bd5b9aea5fb0
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-07-24 21:50:40 +0900

    # OBYEKT HAQIDA MA'LUMOT

--- #32. Dunder methodlar.py
+++ #32. Dunder methodlar.py
@@ -1,54 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 20 23:28:37 2026
+Created on Fri Jul 24 21:33:25 2026
 
 @author: mirzo
 """
 
-# MAXSUS METODLAR
-# Pythonda obyektlar bilan ishlashni yanada qulay qilish uchun bir nechta maxsus metodlar bor. Bu metodlarning nomi ikki pastki chiziq bilan yozilgani uchun, double underscore yoki qisqa qilib dunder metodlar deb ataladi. Dunder metolar yordamida obyektlarga qo'shimcha qulayliklar va vazifalar qo'shishimiz mumkin. Klass yoki obyektga oid dunder metodlar ro'yxatini ko'rish uchun dir() funksiyasidan foydalanamiz:
-
-
->>> dir(Avto)
-['_Avto__num_avto',
- '__class__',
- '__delattr__',
- '__dict__',
- '__dir__',
- '__doc__',
- '__eq__',
- '__format__',
- '__ge__',
- '__getattribute__',
- '__gt__',
- '__hash__',
- '__init__',
- '__init_subclass__',
- '__le__',
- '__lt__',
- '__module__',
- '__ne__',
- '__new__',
- '__reduce__',
- '__reduce_ex__',
- '__repr__',
- '__setattr__',
- '__sizeof__',
- '__str__',
- '__subclasshook__',
- '__weakref__',
- 'make',
- 'model',
- 'narh',
- 'rang',
- 'yil']
-#Dunder metodlardan biz __init__ metodi bilan tanishdik. Bu metod klassdan obyekt yaratishda chaqiriladi va obyektning xususiyatlarini belgilaydi. Ushbu darsimizda esa maxsus metodlarning ba'zilari bilan tanishamiz.
 
 
 
-
-#OBYEKT HAQIDA MA'LUMOT
+## Pythonda obyektlar bilan ishlashni yanada qulay qilish uchun bir nechta maxsus metodlar bor. Bu metodlarning nomi ikki pastki chiziq bilan yozilgani uchun, double underscore yoki qisqa qilib dunder metodlar deb ataladi. Dunder metolar yordamida obyektlarga qo'shimcha qulayliklar va vazifalar qo'shishimiz mumkin. Klass yoki obyektga oid dunder metodlar ro'yxatini ko'rish uchun dir() funksiyasidan foydalanamiz:
+    
+    
+   # OBYEKT HAQIDA MA'LUMOT
 #Obyektga print() yoki str() orqali murojat qilinganda obyekt haqida tushunarli ma'lumot qaytarish uchun __repr__va __str__ metodlaridan foydalanamiz. Tushunarli bo'lishi uchun avvalgi darsimizdagi Avto klassiga qaytamiz:
 
 
@@ -97,156 +61,3 @@ avto1 = Avto("GM","Malibu","Qora",2020,40000)
 print(avto1)
 Natija: Avto: Qora GM Malibu
 Mana endi natijamiz ancha tushunarli ko'rinishda chiqdi.
-
-
-
-
-#OBYEKTLARNI TAQQOSLASH
-#Taqqoslash operatorlari yordamida biz turli obyektlarni solishtirishimiz mumkin. Taqqoslash natijasi mantiqiy qiymat (True yoki False) ko'rinishida bo'ladi. 
-
-
-x,y = 5,10
-print(x>y)
-Natija: False
-Keling endi Avto klassidan 2 ta obyekt yaratamiz, va ularni taqqoslab ko'ramiz:
-
-
-avto1 = Avto("GM","Malibu","Qora",2020,40000)
-avto2 = Avto("GM","Lacetti","Oq",2020,20000)
-avto1>avto2
-Natija: TypeError: '>' not supported between instances of 'Avto' and 'Avto'
-Xatolik. Demak bu ikki obyektni solshtirib bo'lmas ekan. 
-Biz taqqolash operatorlariga murojat qilganimizda, Python obyektlar ichida taqqoslash uchun maxsus metodlarni qidiradi, agar metodlar topilmasa yuqoridagi kabi TypeError qaytaradi. 
-Taqqoslash metodlari quyidagilardan iborat:
-
-Metod
-Operator
-x.__lt__(self,y)
-x<y
-x.__le__(self,y)
-x<=y
-x.__gt__(self,y)
-x>y
-x.__ge__(self,y)
-x>=y
-x.__eq__(self,y)
-x==y
-x.__ne__(self,y)
-x!=y
-Yuqoridagi obyektlardan yarmi uchun metodlar yozishimiz kifoya, misol uchun __lt__ (x<y) metodini yozsak, __gt__ (x>y) metodini yozishimiz shart emas, yoki __le__ metodi, __ge__metodini ham o'z ichiga oladi, va hokazo.
-Keling tushunarli bo'lishi uchun Avto klassiga obyektlarni solishtirish uchun metod yozamiz. Deylik, biz obyektlarni narhi bo'yicha solishtirmoqchimiz, unda klassimizga quyidagi metodlarni qo'shamiz (klassni to'liq yozmadik, faqat qo'shilgan metodlarni keltiramiz):
-
-
-    def __eq__(self,boshqa_avto):
-        """Tenglik"""
-        return self.narh == boshqa_avto.narh
-    
-    def __lt__(self,boshqa_avto):
-        """Kichik"""
-        return self.narh<boshqa_avto.narh
-    
-    def __le__(self,boshqa_avto):
-        """Kichik yoki teng"""
-        return self.narh<=boshqa_avto.narh
-Kodimizga e'tibor qilsangiz biz tenglik (__eq__) yoki kichiklikni (__lt__) tekshirmoqchi bo'lganimizda ikki avtoning aynan narhi bo'yicha solishtiramiz (self.narh == boshqa_avto.narh).
-Mana endi avtolarni solishtirsak bo'ladi:
-
-
-avto1 = Avto("GM","Malibu","Qora",2020,40000)
-avto2 = Avto("GM","Lacetti","Oq",2020,20000)
-print(avto1>avto2)
-Natija: False
-
-
-avto3 = Avto("Honda","Accord","Oq",2017,40000)
-print(avto1==avto3)
-Natija: True
-
-
-
-#OBYEKT UZUNLIGI
-Pythonda len() funksiyasi yordamida turli obyektlarning uzunligini bilishimiz mumkin, misol uchun matn, ro'yxat, lug'at, set va hokazo.
-
-
-matn = 'hello world'
-print(len(matn))
-Natija: 11
-
-
-sonlar = [12, 34, 56, 66]
-print(len(sonlar))
-Natija: 4
-Biz len() funksiyasiga murojat qilganimizda, Python funksiyaga uzatilgan obyektning ichidagi maxsus __len__ metodiga murojat qiladi. Agar bu metod mavjud bo'lmasa dasturimiz xato qaytaradi.
-
-
-len(avto1)
-Natija: TypeError: object of type 'Avto' has no len()
-Misol uchun, bizning Avto klassimizda bu metod yozilmagan, shuning uchun Python TypeError xatosini qaytardi.
-Kelin endi __len__metodini qanday ishlatishga ham misol ko'raylik.
-Boshlanishiga, yangi, AvtoSalon degan klass yaratamiz. Bu klassimiz 2 ta xususiyatga ega: salon nomi (name) va salondagi mashinalar (avtolar).
-
-
-class AvtoSalon:
-    """Avtosalon klassi"""
-    def __init__(self,name):
-        self.name = name
-        self.avtolar = []
-
-    def __repr__(self):
-        return f"{self.name} avtosaloni"
-Yuqoridagi klassdan yangi obyekt yaratamiz:
-
-salon1 = AvtoSalon("MaxAvto")
-print(salon1)
-Natija: MaxAvto avtosaloni
-AvtoSalon klassimizga __repr__metodini qo'shganimiz uchun natijamiz chiroyli ko'rinishda chiqdi.
-Keling endi salonga avtomobil qo'shish uchun yangi add_avto() metodini yozamiz. Bu metodimiz Avto klassiga oid obyektlarni qabul qilishi kerak. add_avto() ga uaztilgan parametr Avto klassiga tegishli yoki yo'qligini tekshirish uchun maxsus isinstance() funksiyasidan foydalanamiz.
-Bu funksiya biror obyekt ma'lum klassga tegishli ekanligini tekshirish uchun ishlatiladi:
-
-
->>> isinstance("salom",str)
-True # "salom" bu str
->>> isinstance(9.5,int)
-False # 9.5 int (butun son) emas
->>> isinstance(avto1,Avto)
-True # avto1 Avto klassiga tegishli
-add_avto() metodiga qaytamiz:
-
-
-class AvtoSalon:
-    """Avtosalon klassi"""
-    def __init__(self,name):
-        self.name = name
-        self.avtolar = []
-
-    def __repr__(self):
-        return f"{self.name} avtosaloni"
-    
-    def add_avto(self,avto):
-        if isinstance(avto,Avto): # agar avto Avto klassidan bo'lsa
-            self.avtolar.append(avto)
-        else:
-            print("Avto obyketini kiriting")
-Metodimizni tekshirib ko'ramiz:
-
-
-# Avto obyektlarini yaratamiz
-avto1 = Avto("GM","Malibu","Qora",2020,40000)
-avto2 = Avto("GM","Lacetti","Oq",2020,20000)
-avto3 = Avto("Toyota",'Carolla',"Silver",2018, 45000)
-
-# Yuqoridagi obyektlarni salon1 ga qo'shamiz
-for avto in [avto1, avto2, avto3]: 
-    salon1.add_avto(avto)
-Mana navbat __len__ metodiga. Biz bu metod yordamida len() funksiyasi salonimizdagi avtomobillar sonini qaytaradigan qilamiz. Buning uchun yuqoridagi AvtoSalon klassiga __len__ funksiyani ham qo'shamiz va uni obyekt ichidagi avtolar ro'yxatyining uzunligini qaytaradigan qilamiz:
-
-
-    def __len__(self):
-        return len(self.avtolar)
-Mana endi bizning AvtoSalon klassimizga oid obyektlar uchun ham len() funksiyasini qo'llasak bo'ladi:
-
-
->>> print(len(salon1))
-3 # Salonimizda 3 ta moshina bor
-
-
