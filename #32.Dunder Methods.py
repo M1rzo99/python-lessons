193515c2ea41d318e01478caf58fa66c2d62aac4
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-07-29 20:17:43 +0900

    DUNDER OBYEKT HAQIDA MA'LUMOT

--- #32.Dunder Methods.py
+++ #32.Dunder Methods.py
@@ -41,3 +41,73 @@ Created on Sun Jul 26 12:53:02 2026
  'narh',
  'rang',
  'yil']
+
+
+
+# Biz dunderlar orqali o'zimizga kerakli bo'lagna methodlarni yozib olsak bo'ladi. 
+# Dunder ayna bizga qulaylik yaratadi. Misol qilib arifmetik amallarni: 1-son 2- sondan kattami,kichikmi,tengmi,teng kattami yoki teng kiechikmi Yoki Tengmi shularni solishitirish mumkin bo'ladi.
+
+# Dunder ningishlashi: Taqqoslash ni berganda,qaysiki  biz ishlatmoqchi bo'lgan methodni,biz yozgan methodlar orasidan birma-bir qidiradi va Keraklisini topib solishtiradi. SHu tariqa dunder methodlar ishalydi
+ 
+
+
+#Dunder methodlaridan biz __init__ methodi bn tanioshdik. BU method classdan obyekt yaratishda chaqirilinadi va Obyejt xususiyatlarini belgilaydi.
+
+#Obyekt haqida ma'lumotlar: 
+    #Obyektga print() yokida str() orqali murojat qilganda obyekt haqida tushunarli ma'lumot qaytarish uchun __repr__ va __str__ methodlaridan foydalanamiz. 
+    # Va asosan __repr__ dan foydalanishni tavfsiya berishadi
+
+# Keling avvalgi darsimizdagi Avto classi orqali bazi misollar ko'ramiz:
+    
+#class Avto:
+ #   __num_avto = 0
+  #  """Avtomobil klassi"""
+  #  def __init__(self,make,model,rang,yil,narx):
+    #    """Avtomobilning xususiyatlari"""
+    #    self.make=make
+    #    self.model=model
+    #    self.rang=rang
+    #    self.yil=yil
+    #    self.narx=narx
+  #      Avto.__num_avto +=1
+  #  """ cycle har bir aylanganligini ko'rsatib beradi"""
+#Yuqoridagi classdan obyekt yaratamiz va obyekt haqida malumot olish un print() functionini chaqiramiz
+#avto1 = Avto("GYM", "Malibu2", "Qora", "2019", 20000)
+#print(avto1)
+
+#Natija: <__main__.Avto object at 0x00000238A6DAE0C8> 
+# Qandaydir tushunarsiz ma'lumot.
+# Gap shundaki biz har gal obyektga print() yoki str() yoki repr() orqali murojat qilganda,Python Obyekt ichida __str__ yoki __repr__ methodlarioga murojat qiladi. 
+#Agar biz bu metholarni yozmagan bo'lsak, yuqorodagi kabi malumot qaytaradi.
+
+#Biz ushbu methodlari yangidan yozib,biz istagan ma'lumotni qaytaradigan qilishimiz mumkin.Odatda Quyidagi ikki methoddan birini yozish kifoya.Odatda __repr__ umumiyroq, __str__ esa batafsilroq ma'lumot olish un ishlatilinadi.
+# Ikkalasidan biri tanlanganida, __repr__ methodi yon bosiladi, sababi bu print(),str() va repr funksiyalarining hammasi bn ishlaydi.
+
+
+class Avto:
+    __num_avto = 0
+    """Avtomobil klassi"""
+    def __init__(self,make,model,rang,yil,narx):
+        """Avtomobilning xususiyatlari"""
+        self.make=make
+        self.model=model
+        self.rang=rang
+        self.yil=yil
+        self.narx=narx
+        Avto.__num_avto +=1
+        
+    def __repr__(self):
+        return f"Avto:Rangi {self.rang},Zavod {self.make} va Markasi {self.model}"
+    """ cycle har bir aylanganligini ko'rsatib beradi"""
+#Yuqoridagi classdan obyekt yaratamiz va obyekt haqida malumot olish un print() functionini chaqiramiz
+avto1 = Avto("GYM", "Malibu2", "Qora", "2019", 20000)
+print(avto1)
+
+
+
+
+
+
+
+
+
