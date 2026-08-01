8000ee92b034f1117228a738866f66972b992cbd
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-08-01 20:46:22 +0900

    # Fayllar bn ishlash: Kirish

---
+++ #33.Fayllar bn ishlash.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  1 19:52:52 2026
+
+@author: Mirzo
+"""
+# Fayllar bn ishlash: Kirish
+# Ushbu bo'limda katta hajmdagi ma;lumoitlarni fayldan yuklab olish va dastur yakunida kerakli ma'lumotlarni va dastur natijasini faylga saqlashni o'rganamiz.
+# Fayllar bn ishlash dastur foydalanuvchilariga ha dasturga o'zlari istagan ma'lumotlarni yuklash imklonini beradi.
+
+
