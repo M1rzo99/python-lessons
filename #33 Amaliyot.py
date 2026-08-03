663d745aa5a857f973f3bbbb467a77885e7e65ac
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-08-03 17:23:39 +0900

    #3.Amaliyot
    Sizning tug'ilgan kuningiz  π soni tarkibida uchraydimi yoki yo'q ekanligini aniqlovchi funksiya yozing. Misol uchun, tug'ilgan sanangiz 25 Fevral, 2000-yil bo'lsa, 25022000 ketma-ketligi yuqoridagi matnda uchraydimi yo'q toping.

--- #33 Amaliyot.py
+++ #33 Amaliyot.py
@@ -6,7 +6,24 @@ Created on Mon Aug  3 17:06:52 2026
 @author: abcd
 """
 
-#1.  Bugun o'rgangan narsalaringizni matnga yozing va matnni Python yordamida oching
+#1.  Bugun o'rgangan narsalaringizni matnga yozing va matnni Python yordamida oching.
 with open("amaliyot.txt") as file:
     info = file.read()
 print(info)
+
+#3. Sizning tug'ilgan kuningiz  π soni tarkibida uchraydimi yoki yo'q ekanligini aniqlovchi funksiya yozing. Misol uchun, tug'ilgan sanangiz 25 Fevral, 2000-yil bo'lsa, 25022000 ketma-ketligi yuqoridagi matnda uchraydimi yo'q toping.
+
+def pi_tekshir(sana):
+    with open("pi_million_digits.txt") as file:
+        pi = file.read().replace("\n", "")
+
+    if sana in pi:
+        return "Topildi ✅"
+    else:
+        return "Topilmadi ❌"
+
+
+# Misol
+tugilgan_sana = input("Tug'ilgan sanangizni DDMMYYYY ko'rinishida kiriting: ")
+print(pi_tekshir(tugilgan_sana))
+
