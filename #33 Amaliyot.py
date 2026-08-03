d50e404fe95f75a19dfdf493f63650061af51c0e
Mirzo Shomuratov | mijoo <accaunoff99@gmail.com>
2026-08-03 17:36:44 +0900

    #4.Amaliyot
    Fayl ichidagi matnni float ma'lumot turiga o'tkazing va pickle yordamida yangi faylga saqlang.

--- #33 Amaliyot.py
+++ #33 Amaliyot.py
@@ -13,17 +13,40 @@ print(info)
 
 #3. Sizning tug'ilgan kuningiz  π soni tarkibida uchraydimi yoki yo'q ekanligini aniqlovchi funksiya yozing. Misol uchun, tug'ilgan sanangiz 25 Fevral, 2000-yil bo'lsa, 25022000 ketma-ketligi yuqoridagi matnda uchraydimi yo'q toping.
 
-def pi_tekshir(sana):
-    with open("pi_million_digits.txt") as file:
-        pi = file.read().replace("\n", "")
+# def pi_tekshir(sana):
+#     with open("pi_million_digits.txt") as file:
+#         pi = file.read().replace("\n", "")
 
-    if sana in pi:
-        return "Topildi ✅"
-    else:
-        return "Topilmadi ❌"
+#     if sana in pi:
+#         return "Topildi ✅"
+#     else:
+#         return "Topilmadi ❌"
 
 
 # Misol
-tugilgan_sana = input("Tug'ilgan sanangizni DDMMYYYY ko'rinishida kiriting: ")
-print(pi_tekshir(tugilgan_sana))
+#tugilgan_sana = input("Tug'ilgan sanangizni DDMMYYYY ko'rinishida kiriting: ")
+#print(pi_tekshir(tugilgan_sana))
 
+
+#4.Fayl ichidagi matnni float ma'lumot turiga o'tkazing va pickle yordamida yangi faylga saqlang.
+
+import pickle
+
+with open("pi.txt") as file:
+    pi = float(file.read().replace("\n", ""))
+
+with open("pi.dat", "wb") as file:
+    pickle.dump(pi, file)
+
+with open("pi.dat", "rb") as file:
+    yangi_pi = pickle.load(file)
+
+print(yangi_pi)
+print(type(yangi_pi))
+    
+
+    
+    
+    
+    
+    
