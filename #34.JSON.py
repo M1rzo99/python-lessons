43e81c8c091b4deee2228c730684caec73e5997b
Mirzo Shomuratov | mijoo <94212669+M1rzo99@users.noreply.github.com>
2026-08-05 18:29:19 +0900

    Pythondan JSON ga

--- #34.JSON.py
+++ #34.JSON.py
@@ -7,7 +7,7 @@ Created on Wed Aug  5 18:05:45 2026
 """
 
 
-# JSON nima? 
+#1. JSON nima? 
 # JSON(JAVA SCRIPT Object Notation) bugungi kunda ma'lumotlarni saqlash va internet orqali uzatish un qo'llanilidigan eng mashhur platforma hisoblanadi.
 # Dastavval Java Script un Yaratilgan bu format, bugungi kunda deyarli barcha dasturlash tillari tomonidan ishlatilinadi.Qolaversa JSON formatdagi fayllarning tarkibini oddiy matn muharriri yordamida ko'rish va tahrirlash mumkin.
 # Aksar hollarda dastur va server orasidagi ma'lumotlar aynan JSON ko'rinishda uzatilinadi.
@@ -27,3 +27,61 @@ Created on Wed Aug  5 18:05:45 2026
    
 # Demak,dsaturimiz davomida malumotlarni JSON ko'rinishdia saqlashmiz, internet orqali boshqa foydalanuvxhilarga ,dasturlarga yoki serverlarga yuborishimiz,JSON fayllarni Pythonda ochib,unga ishlov berishimiz va turli amallar bajarishimiz mn.
 # JSON o'zgaruvchilar, tarkibidan qat'iy nazar matn ko'rinishida saqlanadi.
+
+#2.Pythondan JSON ga
+
+# JSON ma'lumotlar va fayllar bn ishlash un pythonda maxsus json moduli bor. Demak,dasturimiz boshida biz bu modulni yuklab olishmiz kk.
+# malumotlarni JSON matniga o'tkazish un  ikki funksiyasan foydalanamiz:
+    # json.dumps(x) - berilgan x o'zgaruvchisini JSON matniga o'zgartiradi
+    # json.dump(x,fayl) - berilgan x o'zgaruvchini JSON ga o'tkazib,ko'rsatilgan faylga saqlaydi.
+ 
+# json.dumps() - Ma'lumotlarni json formatiga o'tkazish un foydalanamiz: 
+import json
+x=10
+x_json = json.dumps(x)
+
+ism="Mirzo"
+ism_json = json.dumps(ism)
+
+
+sonlar = [12,45,56,78,7]
+sonlar_json = json.dumps(sonlar)
+
+# JSON malumotlarni matn(string) ko'rinishid saqlaydi
+print(type(sonlar_json))
+
+
+
+bemor = {
+  "ism": "Alijon Valiyev",
+  "yosh": 30,
+  "oila": True,
+  "farzandlar": ("Ahmad","Bonu"),
+  "allergiya": None,
+  "dorilar": [
+    {"nomi": "Analgin", "miqdori": 0.5},
+    {"nomi": "Panadol", "miqdori": 1.2}
+  ]
+}
+
+bemor_json = json.dumps(bemor)
+print(bemor_json)
+
+# Yuqridagi natijada o'qish un juda noqulay. Keling uni o'qishga qulay holatga o'tkazamiz. 
+# Buning un bizga dumps() ga qoo'shimcha indent=4 parametrnini beramiz.Bu parametr ma'lumotlarni saqlashda chapdan o'ngga qancha joy tashlash kerakligini aytadi.
+
+bemor_json = json.dumps(bemor, indent=4)
+print(bemor_json)
+
+# Mavzu boshida,JSON ichidagi ma'lumotlar Java Script malumot turlariga konventsiya qilinadi degandik.Buni yuqoridagi misolda korishingiz mumkin(farzandlar,oila,allergiya kalitlari qiymatini asl lugat bn solishtiring)
+
+
+
+
+
+
+
+
+
+
+
