f7cb8f5a71cd1fe26b1b10530ec585927728fa1c
Mirzo Shomuratov | mijoo <94212669+M1rzo99@users.noreply.github.com>
2026-08-05 18:06:44 +0900

    JSON nima?

---
+++ #34.JSON.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 18:05:45 2026
+
+@author: mirzo
+"""
+
+
+# JSON nima? 
+# JSON(JAVA SCRIPT Object Notation) bugungi kunda ma'lumotlarni saqlash va internet orqali uzatish un qo'llanilidigan eng mashhur platforma hisoblanadi.
+# Dastavval Java Script un Yaratilgan bu format, bugungi kunda deyarli barcha dasturlash tillari tomonidan ishlatilinadi.Qolaversa JSON formatdagi fayllarning tarkibini oddiy matn muharriri yordamida ko'rish va tahrirlash mumkin.
+# Aksar hollarda dastur va server orasidagi ma'lumotlar aynan JSON ko'rinishda uzatilinadi.
+
+# JSON yordamida nafaqat lug'at,balki boshqa turdagi ma'lumotlarni ham saqlashimiz mumkin.Bunda Pythondagi malumot turlari,quyidagi jadval asosida,Java Script ma'lumot turlariga konvertasiya qilinadi: 
+    # Python    JavaScript
+    
+ #   dict             Object
+ #   list             Array
+ #   tuple            Array
+  #  str()            String
+ #   int()            Number
+  #  float            Number
+  #  True            true
+   # False           false
+   # None            null
+   
+# Demak,dsaturimiz davomida malumotlarni JSON ko'rinishdia saqlashmiz, internet orqali boshqa foydalanuvxhilarga ,dasturlarga yoki serverlarga yuborishimiz,JSON fayllarni Pythonda ochib,unga ishlov berishimiz va turli amallar bajarishimiz mn.
+# JSON o'zgaruvchilar, tarkibidan qat'iy nazar matn ko'rinishida saqlanadi.
