b7b93c08f2d90c086d9690ed8276e38d3a9fc136
Mirzo Shomuratov | mijoo <94212669+M1rzo99@users.noreply.github.com>
2026-08-09 16:38:00 +0900

    Malum turdagi xatolarni ushalsh

--- #35.xatolar bn ishlash.py
+++ #35.xatolar bn ishlash.py
@@ -36,22 +36,102 @@
 # Yuqoridagi kodimizda biz try moduli ichida xato qaytarishi mumkin bo'lgan ifodani ham (tyil = int(tyil)), xato qaytmaganda bajarilishi kerak bo'lgan ifodani ham (print(f"Siz {2021-tyil} yoshdasiz") ) birdan yozib ketayapmiz. Aslida, bunday qilishimiz to'g'ri emas. 
 # To'g'ri usuli, bu avval xatoga tekshirish va xato yuz bermaganda bajariladigan ifodani alohida, else blokida yozish:
 
-yosh = input("Yoshingizni kiriting: ")
+# yosh = input("Yoshingizni kiriting: ")
 
-try: 
-        yosh = int(yosh)
+# try: 
+#         yosh = int(yosh)
 
-except:
-        print("Butun son kiritmadingiz!")
+# except:
+#         print("Butun son kiritmadingiz!")
 
 
-else:
-        print(f"Siz {2026-yosh} yilda tug'ilgansiz")
+# else:
+#         print(f"Siz {2026-yosh} yilda tug'ilgansiz")
         
-print("Dastur tugadi")     
+# print("Dastur tugadi")     
         
 # Lekin yuqoridagi usul har doim ham qo'l kelavermaydi.
         
         
+# Malum turdagi xatolarni ushalsh
+
+# Xatolarni turlari ko'p, except opertori yordamida esa biz aynan qaysi xatoalrni ushlamoqchi ekanimizni ham ko'rsatib ketishimiz mn.
+# MS: yuqoridagi misolda int()  funksiyasi ValueError xatosini qaytaradi.
+#Agar biz faqatgina shu error ni ushlamoqchi bo'lsak, kodimizini quyidagicha o'zgartiramiz:
+    
+#ValueError
+# yosh = input("Yoshingizni kiriting: ")
+
+# try: 
+#         yosh = int(yosh)
+
+# except ValueError:
+#         print("Butun son kiritmadingiz!")
+
+# else:
+#         print(f"Siz {2026-yosh} yilda tug'ilgansiz")
+        
+# print("Dastur tugadi") 
+
+# ZeroDivisionError - 0 ga bo'lish xatoligi.
+
+x,y=5,10
+try:
+   natija =  y / (x-1)
+    
+except ZeroDivisionError:
+    print("Yuqoridagi sonni 0 ga bo'lib b'lmaydi")
+else:
+    
+    print(f" Natija: {natija}")
+    
+# KeyError - lug'atda mavjud bo'lmaagan kalitga murojat qilishda kelib chiqadi
+
+user={
+ "username":"q13",
+ "status":"admin",
+ "email":"accaunoff99@gmail.com",
+ "phone":"01042276466"
+ }
+key="tel"
+
+try:
+    print(f"Foydalanuvchi: {user[key]}")
+except KeyError:
+    print("Bunday kalit mavjud emas!")
+
+# FileNotFounder - mavjud bo'lmagan fileni so'raganimizda
+
+#Avvalgi darsimizda fayllar bilan ishlashni o'rgangan edik. Fayllarni biz o'qish (open(filename,'r')) yoki yozish (open(filename,'w')) uchun ochishimiz mumkin. Agar faylga ma'lumot yozish uchun ochishda, mavjud bo'lmagan faylga murojat qilsak, Python yangi fayl yaratadi. Lekin, faylni o'qish uchun ochishda fayl nomini xato yozsak, yoki mavjud bo'lmagan faylni ochmoqchi bo'lsak FileNotFoundError (fayl topilmadi) xatoligi yuzaga keladi. 
+
+
+# filename = "data.txt" #bunday file mavjud emas
+# with  open(filename) as f:
+#     text = f.read()
+#  Natija: FileNotFoundError: [Errno 2] No such file or directory: 'data.txt'
+
+# Tepadagi xatolini ushlab qolish un except FileNotFoundError dan foydalanamiz
+
+filename = "data.txt" #bunday file mavjud emas
+try:
+    
+ with  open(filename) as f:
+     text = f.read()
+except FileNotFoundError:
+    print(f"Kechirasiz,{filename} fayli mavjud emas.Boshqa faylni tanlang!")
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+
         
         
