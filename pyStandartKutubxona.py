6235052b036cac6ad00cd079cdb0f2fd85b83335
Mirzo <user@userui-MacBookPro.local>
2026-08-21 13:32:38 +0900

    feat: # Kuchli parolni tekshirish

--- pyStandartKutubxona.py
+++ pyStandartKutubxona.py
@@ -183,5 +183,21 @@ Quyidagi yo'nalishdagi maqolalar qabul qilinadi:
 👉 Aniq va tabiiy fanlarni zamonaviy pedagogik texnologiyalar asosida o‘qitish  metodikasi.
 👉 Umumta’lim  fanlarini o‘qitishda  STEAM yondashuvning o’rni va ahamiyati. """
 andoza = '[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+'
-email = re.findall(andoza,matn)
+email = re.findall(andoza,matn) # findall orqali emailni ajratib olshimiz mn.
 print(email)
+
+# Andoza yordammida foydalanuvhci kiritgan qiymatlarni ham ma'lum shartlarga jb berishini tekshirib olishimmiz mn.
+
+
+# Kuchli parolni tekshirish
+andoza = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$'
+msg = "Yangi parol kiriting"
+msg += '(kamida 8 belgidan iborat, kamida 1 ta lotin bosh harf, 1 ta kichik harf, '
+msg += '1 ta son va 1 ta maxsus belgi boʻlishi kerak): '
+while True:
+    password = input(msg)
+    if re.match(andoza,password):
+        print("Maxfiy so'z qabul qilindi")
+        break
+    else:
+        print("Maxfiy so'z shartlarga mos kelmadi!")
