d5e346fd217cb80295c6b2e54f0df0cf51bad8e0
M1rzo99 <accaunoff99@gmail.com>
2026-08-22 20:39:29 +0900

    feat: BeautifulSoup4

--- 39pipvaTashqiLib.py
+++ 39pipvaTashqiLib.py
@@ -34,9 +34,25 @@ print(f"Tarjima: {tarjima.text}")
 
 # Internetda restcountries.eu sahifasi mavjud. Bu sahifa orqali dunyodagi davlatlar haqida turli maʻlumotlarni olishingiz mumkin. Sahifadan foydalanish qulay boʻlishi uchun esa, sahifa yaratuvchilari bir nechta tayyor API lar eʻlon qilishgan.
 #  Misol uchun Oʻzbekiston haqida maʻlumot olish uchun quyidagi manzilga soʻrov yuborasiz: https://restcountries.eu/rest/v2/name/Uzbekistan÷
+# import requests
+# country = "Uzbekistan"
+# url = f"'https://api.restcountries.com/countries/v5/codes.alpha_2/CA?pretty=1{country}"
+# r = requests.get(url)
+# r_json = r.json()[0]
+# print(r_json['capital'])
+
+# BeautifulSoup4
+#pip install beautifulsoup4 
+
+
+#BeautifulSoup juda kuchli modullardan biri bo'lib, bu modul yordamida turli veb sahifalardan istalgan ma'lumotlarni yig'ishtirib (scarpping) olish mumkin. Biror kishining instagram sahifasidagi barcha rasmlar deysizmi, Facebook guruhidagi barcha postlar va izohlar deysizmi, oldi-sotdi bozoridagi e'lonlar deysizmi, marhamat, bs4 moduli yordamida buni bemalol avtomatlashtirish mumkin. 
+
+#Odatda bs4 moduli requests moduli bilan hamkorlikda ishlaydi. Keling, sodda misol kor'amiz. Avvalgi bo'limda, requests yordamida kun.uz sahifasining html kodini olgan edik. Endi esa bs4 yordamida html sahifadan oxirgi yangiliklarning mavzusini ajratib olamiz.
+
 import requests
-country = "Uzbekistan"
-url = f"'https://api.restcountries.com/countries/v5/codes.alpha_2/CA?pretty=1{country}"
-r = requests.get(url)
-r_json = r.json()[0]
-print(r_json['capital'])
+from bs4 import BeautifulSoup
+url = "https://kun.uz/uz/news/main"
+r  = requests.get(url)
+soup = BeautifulSoup(r.text,"html.parser")
+news = soup.find_all(class_="news-title") # yangiliklar mavzusini ajratib olamiz.
+print(news[0].text) # Birinchi yangiliklarni konsolga chiqaramiz.
