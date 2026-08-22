a9c8c633541fd11c208cfd2ef50fad6a43966d83
M1rzo99 <accaunoff99@gmail.com>
2026-08-22 21:01:58 +0900

    feat: fuzzywuzzy

--- 39pipvaTashqiLib.py
+++ 39pipvaTashqiLib.py
@@ -145,37 +145,52 @@ import requests
 #wxPython paketi deyarli barcha operatsion tizimlarda ishlaydigan grafik interfeysli dasturlar yaratish uchun mo'ljallangan. Bu paket haqida alohida darsliklar qilish mumkin, shuning uchun biz faqatgina bitta misol bilan chegralanamiz.
 
 
-import wx
-from googletrans import Translator
+# import wx
+# from googletrans import Translator
 
-tarjimon = Translator()
-class MyFrame(wx.Frame):    
-    def __init__(self):
-        super().__init__(parent=None, title='Oʻzbek-Ingliz Lugʻat')
-        panel = wx.Panel(self)        
-        my_sizer = wx.BoxSizer(wx.VERTICAL)        
-        self.text_ctrl = wx.TextCtrl(panel)
-        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
+# tarjimon = Translator()
+# class MyFrame(wx.Frame):    
+#     def __init__(self):
+#         super().__init__(parent=None, title='Oʻzbek-Ingliz Lugʻat')
+#         panel = wx.Panel(self)        
+#         my_sizer = wx.BoxSizer(wx.VERTICAL)        
+#         self.text_ctrl = wx.TextCtrl(panel)
+#         my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
         
-        my_btn = wx.Button(panel, label='TARJIMA')
-        my_btn.Bind(wx.EVT_BUTTON, self.on_press)
-        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
+#         my_btn = wx.Button(panel, label='TARJIMA')
+#         my_btn.Bind(wx.EVT_BUTTON, self.on_press)
+#         my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
         
-        self.text_out = wx.TextCtrl(panel,style = wx.TE_READONLY)        
-        my_sizer.Add(self.text_out, 0, wx.ALL | wx.EXPAND, 5)         
-        panel.SetSizer(my_sizer)        
-        self.Show()
+#         self.text_out = wx.TextCtrl(panel,style = wx.TE_READONLY)        
+#         my_sizer.Add(self.text_out, 0, wx.ALL | wx.EXPAND, 5)         
+#         panel.SetSizer(my_sizer)        
+#         self.Show()
 
-    def on_press(self, event):
-        value = self.text_ctrl.GetValue()
-        if not value:                       
-            self.text_out.SetValue("Soʻz kiritmadingiz")
-        else:
-            tarjima = tarjimon.translate(value, src='uz', dest='en')
-            self.text_out.SetValue(tarjima.text.capitalize()) 
+#     def on_press(self, event):
+#         value = self.text_ctrl.GetValue()
+#         if not value:                       
+#             self.text_out.SetValue("Soʻz kiritmadingiz")
+#         else:
+#             tarjima = tarjimon.translate(value, src='uz', dest='en')
+#             self.text_out.SetValue(tarjima.text.capitalize()) 
     
 
-if __name__ == '__main__':
-    app = wx.App()
-    frame = MyFrame()
-    app.MainLoop()
+# if __name__ == '__main__':
+#     app = wx.App()
+#     frame = MyFrame()
+#     app.MainLoop()
+
+# fuzzywuzzy 
+
+# Bu modul yordamida so'zlarni bir-biriga solishtirish yoki matnlar tarkibida kerakli so'zni topishda foydalanamiz. 
+
+#Quyidagi misolda "salom" so'zini "assalom" va "salim" so'zlari bilan naqadar o'xshashligini tekshrib ko'ramiz:
+
+
+from fuzzywuzzy import process
+from uzwords import words
+
+# Matnlar orasidan 3 ta eng o'xshashlarini ajratib olish
+text = "салом"
+matches = process.extract(text, words, limit=3)
+print(matches)
